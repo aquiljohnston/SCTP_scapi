bbd0a3b8076162dd2cb88fb3957b41a327834fcf
Michael Davis <mdavis@speardom.com>
2015-10-27 17:21:50 -0400

    Initial project creation.

---
+++ SCAPI/scapi/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlsrv:Server=192.168.20.46;Database=SC1-DEV02',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
