1e5e898b944fe045c34d7d7376fdf66396b4aeea
unknown <yfadhil@EDebass-PC.speardom.com>
2015-12-16 13:37:05 -0500

    MileageCard created model and controller

---
+++ SCAPI/scapi/controllers/MileageCardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\MileageCard;
+use app\authentication\BaseActiveController;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use yii\web\Response;
+
+/**
+ * MileageCardController implements the CRUD actions for MileageCard model.
+ */
+class MileageCardController extends BaseActiveController
+{
+    public $modelClass = 'app\models\MileageCard'; 
+	public $mileageCard;
+	 
+	public function actions()
+	{
+		$actions = parent::actions();
+		unset($actions['view']);
+		return $actions;
+	}
+
+    /**
+     * Displays a single MileageCard model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        $mileageCard = MileageCard::findOne($id);
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = $mileageCard;
+		
+		return $response;
+    }
+
+   
+}
