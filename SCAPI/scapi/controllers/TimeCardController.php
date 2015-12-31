5a72ad87f1ee186b63432f0c065a14b909093c63
Michael Davis <mdavis@speardom.com>
2015-12-31 14:59:30 -0500

    small fixes to timecard and user controllers added timecard model

--- SCAPI/scapi/controllers/TimeCardController.php
+++ SCAPI/scapi/controllers/TimeCardController.php
@@ -25,54 +25,14 @@ class TimeCardController extends BaseActiveController
 		return $actions;
 	}
 	
-    public function actionIndex()
-    {
-        $dataProvider = new ActiveDataProvider([
-            'query' => TimeCard::find(),
-        ]);
-    }
-	
     public function actionView($id)
     {
 		$timeCard = TimeCard::findOne($id);
 		$response = Yii::$app ->response;
 		$response -> format = Response::FORMAT_JSON;
 		$response -> data = $timeCard;
+		
 		return $response;
 	}
-
-   /*  public function actionCreate(id)
-    {
-		//Todo id is for the user
-        $timeCard = new TimeCard();
-		$dateToday = time();
-		echo "Create TimeCard" ;
-		
-        $timeCard['TimeCardStartDate'] = $dateToday;
-		
-		$timeCard -> save();
-    } */
-
-   /*  public function actionDelete($id)
-    {
-        $timeCard = TimeCard::findOne($id);
-		$timeCard -> delete(); 
-    } */
 	
-	/* public function actionUpdate($id)
-    {
-		$timeCard = TimeCard::findOne($id);
-		$timeCard['TimeCardProjectID'] = 1;
-		$timeCard -> save();
-    } */
-
-    
-    /* protected function findModel($id)
-    {
-        if (($model = TimeCard::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    } */
 }
