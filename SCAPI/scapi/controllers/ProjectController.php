b75d78dce30ac4113f16b1efea4d027963f390b6
Michael Davis <mdavis@speardom.com>
2016-01-13 16:50:48 -0500

    Updated client and project controllers to be active controllers to allow rest calls

--- SCAPI/scapi/controllers/ProjectController.php
+++ SCAPI/scapi/controllers/ProjectController.php
@@ -5,8 +5,8 @@ namespace app\controllers;
 use Yii;
 use app\models\Project;
 use app\models\SCUser;
+use app\controllers\BaseActiveController;
 use yii\data\ActiveDataProvider;
-use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\Response;
@@ -17,120 +17,30 @@ use yii\filters\auth\TokenAuth;
 /**
  * ProjectController implements the CRUD actions for Project model.
  */
-class ProjectController extends Controller
+class ProjectController extends BaseActiveController
 {
-    public function behaviors()
+	public $modelClass = 'app\models\Project'; 
+	
+	public function actions()
+	{
+		$actions = parent::actions();
+		unset($actions['view']);
+		return $actions;
+	}
+
+	public function actionView($id)
     {
-		$behaviors = parent::behaviors();
-		$behaviors['verbs'] = 
-			[
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ];
-		return $behaviors;
-    }
-
-    /**
-     * Lists all Project models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $dataProvider = new ActiveDataProvider([
-            'query' => Project::find(),
-        ]);
-
-        return $this->render('index', [
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single Project model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Project model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Project();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ProjectID]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing Project model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ProjectID]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Project model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Project model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Project the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Project::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+		$project = Project::findOne($id);
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = $project;
+		
+		return $response;
+	} 
 	
 	//return json array of all users attached to a specific project ID
 	public function actionViewAllUsers($projectID)
 	{
-		//$criteria->select = new CDbCriteria();
-		//$criteria->condition = "equipmentProject = $projectID";
-		//$equipArray = Equipment::findAll($criteria);
 		$project = Project::findOne($projectID);
 		$userArray = $project->users;
 		$userData = array_map(function ($model) {return $model->attributes;},$userArray);
