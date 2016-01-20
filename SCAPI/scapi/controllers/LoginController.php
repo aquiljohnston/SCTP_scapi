9d2a3db5962b992148d174da106dd33fa96bb744
aquiljohston <aquiljohnston3@gmail.com>
2016-01-20 14:24:22 -0500

    Added response code to failed login

--- SCAPI/scapi/controllers/LoginController.php
+++ SCAPI/scapi/controllers/LoginController.php
@@ -94,6 +94,7 @@ class LoginController extends ActiveController
 		} else 
 		{
 			$response->data = "Password is invalid.";
+			$response->serStatusCode(401);
 			return $response;
 			Yii::trace('Password is invalid.');
 		}
