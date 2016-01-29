60a4f23ac3e171befd24aa31a85cdae50d7b36d1
Michael Davis <mdavis@speardom.com>
2016-01-29 16:42:01 -0500

    Added Mileage Entry Create Route

--- SCAPI/scapi/controllers/MileageEntryController.php
+++ SCAPI/scapi/controllers/MileageEntryController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use app\models\MileageEntry;
+use app\models\SCUser;
 use app\controllers\BaseActiveController;
 use yii\data\ActiveDataProvider;
 use yii\web\NotFoundHttpException;
@@ -32,6 +33,41 @@ class MileageEntryController extends BaseActiveController
 		$response ->format = Response::FORMAT_JSON;
 		$response->data = $mileage;
 		
+		return $response;
+	}
+	
+	public function actionCreate()
+	{
+		$post = file_get_contents("php://input");
+		$data = json_decode($post, true);
+
+		$model = new MileageEntry(); 
+		$model->attributes = $data;  
+		
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		
+		//created by
+		if ($user = SCUser::findOne(['UserID'=>$model->MileageEntryCreatedBy]))
+		{
+			$fname = $user->UserFirstName;
+			$lname = $user->UserLastName;
+			$model->MileageEntryCreatedBy = $lname.", ".$fname;
+		}
+		
+		//create date
+		$model->MileageEntryCreateDate = date('Y-m-d H:i:s');
+		
+		if($model-> save())
+		{
+			$response->setStatusCode(201);
+			$response->data = $model; 
+		}
+		else
+		{
+			$response->setStatusCode(400);
+			$response->data = "Http:400 Bad Request";
+		}
 		return $response;
 	}
 }
