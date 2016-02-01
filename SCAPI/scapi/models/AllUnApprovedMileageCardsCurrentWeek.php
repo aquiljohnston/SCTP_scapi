502c167f60bbf336f27165b46f3211e879be9a0b
Michael Davis <mdavis@speardom.com>
2016-02-01 16:32:22 -0500

    Add Create Mileage Entry Add View Mileage Entries by Card Add route calls to Mileage and Time Card Views

---
+++ SCAPI/scapi/models/AllUnApprovedMileageCardsCurrentWeek.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "AllUnApprovedMileageCardsCurrentWeek_vw".
+ *
+ * @property integer $UserID
+ * @property string $UserName
+ * @property string $UserFirstName
+ * @property string $UserLastName
+ * @property integer $MileageCardID
+ * @property string $MileageStartDate
+ * @property string $MileageEndtDate
+ * @property integer $MileageCardBusinessMiles
+ * @property integer $MileageCardPersonalMiles
+ * @property integer $MileageCardAllMileage_calc
+ * @property string $MileageCardApprovedBy
+ * @property string $MileageCardApprove
+ * @property string $UserStatus
+ */
+class AllUnApprovedMileageCardsCurrentWeek extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'AllUnApprovedMileageCardsCurrentWeek_vw';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['UserID', 'MileageCardID', 'UserStatus'], 'required'],
+            [['UserID', 'MileageCardID', 'MileageCardBusinessMiles', 'MileageCardPersonalMiles', 'MileageCardAllMileage_calc'], 'integer'],
+            [['UserName', 'UserFirstName', 'UserLastName', 'MileageCardApprovedBy', 'MileageCardApprove', 'UserStatus'], 'string'],
+            [['MileageStartDate', 'MileageEndtDate'], 'safe']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'UserID' => 'User ID',
+            'UserName' => 'User Name',
+            'UserFirstName' => 'User First Name',
+            'UserLastName' => 'User Last Name',
+            'MileageCardID' => 'Mileage Card ID',
+            'MileageStartDate' => 'Mileage Start Date',
+            'MileageEndtDate' => 'Mileage Endt Date',
+            'MileageCardBusinessMiles' => 'Mileage Card Business Miles',
+            'MileageCardPersonalMiles' => 'Mileage Card Personal Miles',
+            'MileageCardAllMileage_calc' => 'Mileage Card All Mileage Calc',
+            'MileageCardApprovedBy' => 'Mileage Card Approved By',
+            'MileageCardApprove' => 'Mileage Card Approve',
+            'UserStatus' => 'User Status',
+        ];
+    }
+}
