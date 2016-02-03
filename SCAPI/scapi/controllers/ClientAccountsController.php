e7f1f6d489ff8f00df3bd1f9686d980642fe3926
aquiljohston <aquiljohnston3@gmail.com>
2016-02-03 11:02:47 -0500

    Added Client Accounts Dropdown Call

---
+++ SCAPI/scapi/controllers/ClientAccountsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\ClientAccounts;
+use app\controllers\BaseActiveController;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+class ClientAccountsController extends BaseActiveController
+{
+	public $modelClass = 'app\models\ClientAccounts'; 
+	
+	
+	//return a json containing pairs of ClientAccountIDs and ClientNames
+	public function actionGetClientAccountDropdowns()
+	{	
+        $clientAccounts = ClientAccounts::find()
+			->all();
+		$namePairs = [];
+		$clientSize = count($clientAccounts);
+		
+		for($i=0; $i < $clientSize; $i++)
+		{
+			$namePairs[$clientAccounts[$i]->ClientAccountID]= $clientAccounts[$i]->ClientAccountName;
+		}
+			
+		
+		$response = Yii::$app ->response;
+		$response -> format = Response::FORMAT_JSON;
+		$response -> data = $namePairs;
+		
+		return $response;
+	}
+}
