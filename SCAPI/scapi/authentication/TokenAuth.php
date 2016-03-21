5de49ef28f40a9e66313627b2d2de44e5aa4bb12
Michael Davis <mdavis@speardom.com>
2016-03-21 14:16:32 -0400

    Fixed a bug with auth token not being checked against correct DB

--- SCAPI/scapi/authentication/TokenAuth.php
+++ SCAPI/scapi/authentication/TokenAuth.php
@@ -5,6 +5,7 @@ namespace  app\authentication;
 use Yii;
 use yii\filters\auth\AuthMethod;
 use app\models\SCUser;
+use app\models\Auth;
 use yii\base\ErrorException;
 
 class TokenAuth extends AuthMethod
@@ -14,6 +15,9 @@ class TokenAuth extends AuthMethod
     public function authenticate($user, $request, $response)
     {
         $token = $request->getAuthUser();
+		$headers = getAllHeaders();
+		SCUser::setClient($headers['X-Client']);
+		Auth::setClient($headers['X-Client']);
 		
 		if ($token !== null) {
 			Yii::$app->user->checkTimeout($token);
