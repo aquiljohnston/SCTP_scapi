403a92e3dfb6a026efffefecc1f588e2a214cde3
aquiljohston <aquiljohnston3@gmail.com>
2016-04-04 15:35:29 -0400

    Updated User GetMe Route Updated Equipment and Project Models Refactored some API Classes

--- SCAPI/scapi/controllers/AppRolesController.php
+++ SCAPI/scapi/controllers/AppRolesController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\AppRoles;
 use app\controllers\BaseActiveController;
+use app\authentication\TokenAuth;
 use yii\data\ActiveDataProvider;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -17,7 +18,69 @@ class AppRolesController extends BaseActiveController
 {
 	public $modelClass = 'app\models\AppRoles'; 
 
+	public function behaviors()
+	{
+		$behaviors = parent::behaviors();
+		//Implements Token Authentication to check for Auth Token in Json  Header
+		$behaviors['authenticator'] = 
+		[
+			'class' => TokenAuth::className(),
+		];
+		$behaviors['verbs'] = 
+			[
+                'class' => VerbFilter::className(),
+                'actions' => [
+					'get-roles-dropdowns'  => ['get'],
+                ],  
+            ];
+		return $behaviors;	
+	}
 	
+	public function actions()
+	{
+		$actions = parent::actions();
+		unset($actions['view']);
+		unset($actions['create']);
+		unset($actions['update']);
+		unset($actions['delete']);
+		return $actions;
+	}
+	
+	public function actionView()
+	{
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = "Method Not Allowed";
+		$response->setStatusCode(405);
+		return $response;
+	}
+	
+	public function actionCreate()
+	{
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = "Method Not Allowed";
+		$response->setStatusCode(405);
+		return $response;
+	}
+	
+	public function actionUpdate()
+	{
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = "Method Not Allowed";
+		$response->setStatusCode(405);
+		return $response;
+	}
+	
+	public function actionDelete()
+	{
+		$response = Yii::$app->response;
+		$response ->format = Response::FORMAT_JSON;
+		$response->data = "Method Not Allowed";
+		$response->setStatusCode(405);
+		return $response;
+	}
 	
 	//return a json containing pairs of AppRoleNames
 	public function actionGetRolesDropdowns()
