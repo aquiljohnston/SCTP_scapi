a9cb7153a96700d2ae0d01498ac7c37a6ca69923
Michael Davis <mdavis@speardom.com>
2016-06-30 09:14:37 -0400

    updated activity model to reflect changes on db

--- SCAPI/scapi/models/Activity.php
+++ SCAPI/scapi/models/Activity.php
@@ -16,15 +16,31 @@ use Yii;
  * @property integer $ActivityPayCode
  * @property double $ActivityLatitude
  * @property double $ActivityLongitude
+ * @property string $ActivityStartDateTime
+ * @property string $ActivityEndDateTime
+ * @property string $ActivityArchiveFlag
  * @property string $ActivityCreateDate
- * @property string $ActivityCreatedBy
+ * @property integer $ActivityCreatedUserUID
  * @property string $ActivityModifiedDate
- * @property string $ActivityModifiedBy
+ * @property integer $ActivityModifiedUserUID
+ * @property string $ActivityUID
+ * @property integer $ActivityProjectID
+ * @property string $ActivitySourceID
+ * @property string $ActivitySrvDTLT
+ * @property string $ActivitySrvDTLTOffset
+ * @property string $ActivitySrcDTLT
+ * @property string $ActivityGPSType
+ * @property string $ActivityGPSSentence
+ * @property string $ActivityShape
+ * @property string $ActivityComments
+ * @property string $ActivityBatteryLevel
+ * @property string $ActivityRevisionComments
+ * @property string $ActivityElapsedSec
  *
  * @property MileageEntryTb[] $mileageEntryTbs
  * @property TimeEntryTb[] $timeEntryTbs
  */
-class Activity extends BaseActiveRecord
+class Activity extends \yii\db\ActiveRecord
 {
     /**
      * @inheritdoc
@@ -40,10 +56,10 @@ class Activity extends BaseActiveRecord
     public function rules()
     {
         return [
-            [['ActivityStartTime', 'ActivityEndTime', 'ActivityCreateDate', 'ActivityModifiedDate'], 'safe'],
-            [['ActivityTitle', 'ActivityBillingCode'], 'string'],
-            [['ActivityCode', 'ActivityPayCode' , 'ActivityCreatedBy', 'ActivityModifiedBy'], 'integer'],
-			[['ActivityLatitude', 'ActivityLongitude'], 'number']
+            [['ActivityStartTime', 'ActivityEndTime', 'ActivityStartDateTime', 'ActivityEndDateTime', 'ActivityCreateDate', 'ActivityModifiedDate', 'ActivitySrvDTLT', 'ActivitySrvDTLTOffset', 'ActivitySrcDTLT'], 'safe'],
+            [['ActivityTitle', 'ActivityBillingCode', 'ActivityArchiveFlag', 'ActivityUID', 'ActivitySourceID', 'ActivityGPSType', 'ActivityGPSSentence', 'ActivityShape', 'ActivityComments', 'ActivityBatteryLevel', 'ActivityRevisionComments', 'ActivityElapsedSec'], 'string'],
+            [['ActivityCode', 'ActivityPayCode', 'ActivityCreatedUserUID', 'ActivityModifiedUserUID', 'ActivityProjectID'], 'integer'],
+            [['ActivityLatitude', 'ActivityLongitude'], 'number']
         ];
     }
 
@@ -60,16 +76,32 @@ class Activity extends BaseActiveRecord
             'ActivityBillingCode' => 'Activity Billing Code',
             'ActivityCode' => 'Activity Code',
             'ActivityPayCode' => 'Activity Pay Code',
+            'ActivityLatitude' => 'Activity Latitude',
+            'ActivityLongitude' => 'Activity Longitude',
+            'ActivityStartDateTime' => 'Activity Start Date Time',
+            'ActivityEndDateTime' => 'Activity End Date Time',
+            'ActivityArchiveFlag' => 'Activity Archive Flag',
             'ActivityCreateDate' => 'Activity Create Date',
-            'ActivityCreatedBy' => 'Activity Created By',
+            'ActivityCreatedUserUID' => 'Activity Created User Uid',
             'ActivityModifiedDate' => 'Activity Modified Date',
-            'ActivityModifiedBy' => 'Activity Modified By',
-			'ActivityLatitude' => 'Activity Latitude',
-			'ActivityLongitude' => 'Activity Longitude',
+            'ActivityModifiedUserUID' => 'Activity Modified User Uid',
+            'ActivityUID' => 'Activity Uid',
+            'ActivityProjectID' => 'Activity Project ID',
+            'ActivitySourceID' => 'Activity Source ID',
+            'ActivitySrvDTLT' => 'Activity Srv Dtlt',
+            'ActivitySrvDTLTOffset' => 'Activity Srv Dtltoffset',
+            'ActivitySrcDTLT' => 'Activity Src Dtlt',
+            'ActivityGPSType' => 'Activity Gpstype',
+            'ActivityGPSSentence' => 'Activity Gpssentence',
+            'ActivityShape' => 'Activity Shape',
+            'ActivityComments' => 'Activity Comments',
+            'ActivityBatteryLevel' => 'Activity Battery Level',
+            'ActivityRevisionComments' => 'Activity Revision Comments',
+            'ActivityElapsedSec' => 'Activity Elapsed Sec',
         ];
     }
-
-    /**
+	
+	/**
      * @return \yii\db\ActiveQuery
      */
     public function getMileageEntryTbs()
