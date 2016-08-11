15ef0fabe78056025f9cfb8ac53c6312b5458d94
Michael Davis <mdavis@southerncrossinc.com>
2016-08-11 15:53:31 -0400

    Implemented pge and scana modules and copied pge controllers into module

--- SCAPI/scapi/modules/v1/Module.php
+++ SCAPI/scapi/modules/v1/Module.php
@@ -7,5 +7,14 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+		
+		$this->modules = [
+			'pge' => [
+				'class' => 'app\modules\v1\modules\pge\Module',
+			],
+			'scana' => [
+				'class' => 'app\modules\v1\modules\scana\Module',
+			],
+		];
     }
 }
