70c7f0908d60433c17394c18115d9c5dc70ab67e
Michael Davis <mdavis@southerncrossinc.com>
2016-08-25 12:00:47 -0400

    Added route call for dispatch table and for assigning IRs

--- SCAPI/scapi/modules/v1/modules/pge/controllers/DispatchController.php
+++ SCAPI/scapi/modules/v1/modules/pge/controllers/DispatchController.php
@@ -3,16 +3,22 @@
 namespace app\modules\v1\modules\pge\controllers;
 
 use Yii;
-use yii\web\Controller;
+use yii\rest\Controller;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\authentication\TokenAuth;
+use app\modules\v1\models\BaseActiveRecord;
 use app\modules\v1\controllers\BaseActiveController;
+use app\modules\v1\modules\pge\models\WebManagementDispatch;
+use app\modules\v1\modules\pge\models\WebManagementUsers;
+use app\modules\v1\modules\pge\models\AssignedWorkQueue;
 use yii\web\ForbiddenHttpException;
 use yii\web\BadRequestHttpException;
 
 class DispatchController extends Controller 
 {
+	
+	public $modelClass = 'app\modules\v1\modules\pge\models\PGEUser'; 
 
 	public function behaviors()
 	{
@@ -27,7 +33,8 @@ class DispatchController extends Controller
                 'actions' => [
 					'get-unassigned' => ['get'],
 					'get-assigned' => ['get'],
-					'get-surveyors' => ['get']
+					'get-surveyors' => ['get'],
+					'dispatch' => ['post'],
                 ],  
             ];
 		return $behaviors;	
@@ -37,100 +44,56 @@ class DispatchController extends Controller
 	{
 		try
 		{
-			$asset1 = [];
-			$asset1["Division"] = "Diablo";
-			$asset1["Work Center"] = "Izual";
-			$asset1["Survey Type"] = "3 YR";
-			$asset1["Map/Plat"] = "161-30-5-C";
-			$asset1["Notification ID"] = "12345678";
-			$asset1["Compliance Date"] = "08/15/2016";
-			$asset1["SAP Released"] = "01/01/2016";
-			$asset1["Assigned"] = "0";
 			
-			$asset2 = [];
-			$asset2["Division"] = "Malthael";
-			$asset2["Work Center"] = "Urzael";
-			$asset2["Survey Type"] = "5 YR";
-			$asset2["Map/Plat"] = "120-31-6-F";
-			$asset2["Notification ID"] = "13572468";
-			$asset2["Compliance Date"] = "08/20/2016";
-			$asset2["SAP Released"] = "01/01/2016";
-			$asset2["Assigned"] = "0";
+			$assetQuery = WebManagementDispatch::find()->where(['Assigned' => 0]);
 			
-			$asset3 = [];
-			$asset3["Division"] = "Belial";
-			$asset3["Work Center"] = "Zoltun Kulle";
-			$asset3["Survey Type"] = "1 YR";
-			$asset3["Map/Plat"] = "133-34-4-C";
-			$asset3["Notification ID"] = "24681357";
-			$asset3["Compliance Date"] = "09/05/2016";
-			$asset3["SAP Released"] = "01/01/2016";
-			$asset3["Assigned"] = "0";
-			
-			$asset4 = [];
-			$asset4["Division"] = "Azmodan";
-			$asset4["Work Center"] = "Cydaea";
-			$asset4["Survey Type"] = "3 YR";
-			$asset4["Map/Plat"] = "141-31-3-C";
-			$asset4["Notification ID"] = "12563478";
-			$asset4["Compliance Date"] = "09/12/2016";
-			$asset4["SAP Released"] = "01/01/2016";
-			$asset4["Assigned"] = "0";
-			
-			$asset5 = [];
-			$asset5["Division"] = "Diablo";
-			$asset5["Work Center"] = "Izual";
-			$asset5["Survey Type"] = "Special";
-			$asset5["Map/Plat"] = "161-30-3-C";
-			$asset5["Notification ID"] = "23146758";
-			$asset5["Compliance Date"] = "09/28/2016";
-			$asset5["SAP Released"] = "01/01/2016";
-			$asset5["Assigned"] = "0";
-			
-			$assets = [];
-			$assets[] = $asset1;
-			$assets[] = $asset2;
-			$assets[] = $asset3;
-			$assets[] = $asset4;
-			$assets[] = $asset5;
-			$assetCount = count($assets);
-			
-			$data = [];
-			
-			//filter assets
-			for($i = 0; $i < $assetCount; $i++)
+			if($division != null)
 			{
-				if($filter == null || stripos($assets[$i]["Division"], $filter) !== false || stripos($assets[$i]["Work Center"], $filter) !== false
-				|| stripos($assets[$i]["Survey Type"], $filter) !== false || stripos($assets[$i]["Map/Plat"], $filter) !== false
-				|| stripos($assets[$i]["Notification ID"], $filter) !== false || stripos($assets[$i]["Compliance Date"], $filter) !== false
-				|| stripos($assets[$i]["SAP Released"], $filter) !== false || stripos($assets[$i]["Assigned"], $filter) !== false)
-				{
-					if($division == null || $assets[$i]["Division"] == $division)
-					{
-						if($workCenter == null || $assets[$i]["Work Center"] == $workCenter)
-						{
-							if($mapPlat == null || $assets[$i]["Map/Plat"] == $mapPlat)
-							{
-								if($surveyType == null || $assets[$i]["Survey Type"] == $surveyType)
-								{
-									$dateArray = explode("/", $assets[$i]["Compliance Date"]);
-									$month = $dateArray[0];
-									//compliance month
-									if($complianceMonth == null || $month == $complianceMonth)
-									{
-										$data[] = $assets[$i];
-									}
-								}
-							}
-						}
-					}
-				}
+				$assetQuery->andWhere(['Division'=>$division]);
 			}
+			
+			if($workCenter != null)
+			{
+				$assetQuery->andWhere(['WorkCenter'=>$workCenter]);
+			}
+			
+			if($mapPlat != null)
+			{
+				$assetQuery->andWhere(['MapPlat'=>$mapPlat]);
+			}
+			
+			if($surveyType != null)
+			{
+				$assetQuery->andWhere(['SurveyType'=>$surveyType]);
+			}
+			
+			if($complianceMonth != null)
+			{
+				$assetQuery->andWhere(['ComplianceYearMonth'=>$complianceMonth]);
+			}
+			
+			if($filter != null)
+			{
+				$assetQuery->andFilterWhere([
+				'or',
+				['like', 'Division', $filter],
+				['like', 'WorkCenter', $filter],
+				['like', 'SurveyType', $filter],
+				['like', 'MapPlat', $filter],
+				['like', 'Notification ID', $filter],
+				['like', 'ComplianceDueDate', $filter],
+				['like', 'SAP Released', $filter],
+				['like', 'Assigned', $filter],
+				['like', 'ComplianceYearMonth', $filter],
+				]);
+			}
+			
+			$assets = $assetQuery->all();
 			
 			//send response
 			$response = Yii::$app->response;
 			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
+			$response->data = $assets;
 			return $response;
 		}
         catch(ForbiddenHttpException $e)
@@ -254,48 +217,80 @@ class DispatchController extends Controller
         }
 	}
 	
-	public function actionGetSurveyors(/*$surveyor = null,*/$filter = null)
+	public function actionGetSurveyors($filter = null)
 	{
 		try
 		{
-			$surveyor1 =[];
-			$surveyor1["Name"] = "Doe, John";
-			$surveyor1["LANID"] = "J0D1";
+			//TODO need to add a new column to the view with lastname, firstname
+			$userQuery = WebManagementUsers::find();
 			
-			$surveyor2 =[];
-			$surveyor2["Name"] = "Doe, Jane";
-			$surveyor2["LANID"] = "J0D2";
-			
-			$surveyor3 =[];
-			$surveyor3["Name"] = "Smith, Bob";
-			$surveyor3["LANID"] = "B2S4";
-			
-			$surveyor4 =[];
-			$surveyor4["Name"] = "Milstone, Fred";
-			$surveyor4["LANID"] = "F3M4";
-			
-			$surveyors = [];
-			$surveyors[] = $surveyor1;
-			$surveyors[] = $surveyor2;
-			$surveyors[] = $surveyor3;
-			$surveyors[] = $surveyor4;
-			$surveyorCount = count($surveyors);
-			
-			$data = [];
-			
-			//filter surveyors
-			for($i = 0; $i < $surveyorCount; $i++)
+			if($filter != null)
 			{
-				if($filter == null || stripos($surveyors[$i]["Name"], $filter) !== false || stripos($surveyors[$i]["LANID"], $filter) !== false)
+				$userQuery->andFilterWhere([
+				'or',
+				['like', 'LastName', $filter],
+				['like', 'UserLANID', $filter],
+				]);
+			}
+			
+			$users = $userQuery->all();
+			
+			//send response
+			$response = Yii::$app->response;
+			$response->format = Response::FORMAT_JSON;
+			$response->data = $users;
+			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+            throw new \yii\web\HttpException(400);
+        }
+	}
+	
+	public function actionDispatch()
+	{
+		try
+		{
+			$post = file_get_contents("php://input");
+			$data = json_decode($post, true);
+			$responseData = [];
+			
+			$assetCount = count($data['Assignments']);
+			
+			for($i = 0; $i < $assetCount; $i++)
+			{
+				BaseActiveRecord::setClient('CometTracker');
+				$assignment = new AssignedWorkQueue;
+				$assignment->SourceID = $data['SourceID'];
+				$assignment->DispatchMethod = 'Dispatched';
+				$assignment->AssignedDate = BaseActiveController::getDate();
+				$assignment->CreatedUserUID = BaseActiveController::getUserFromToken()->UserUID;
+				$assignment->ProjectID = 1;
+				$assignment->ActiveFlag = 1;
+				$assignment->Revision = 0;
+				$assignment->ModifiedUserUID = BaseActiveController::getUserFromToken()->UserUID;
+				$assignment->AssignedWorkQueueUID = BaseActiveController::generateUID('AssignedWorkQueue', $data['SourceID']);
+				$assignment->AssignedInspectionRequestUID = $data['Assignments'][$i]['IR'];
+				$assignment->AssignedUserUID = $data['Assignments'][$i]['User'];
+				AssignedWorkQueue::setClient('pgedev');
+				if($assignment->save())
 				{
-					$data[] = $surveyors[$i];
+					$responseData[] = $assignment;
 				}
+				// else
+				// {
+					// print_r($assignment->getErrors());
+				// }
 			}
 			
 			//send response
 			$response = Yii::$app->response;
 			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
+			$response->data = $responseData;
 			return $response;
 		}
         catch(ForbiddenHttpException $e)
