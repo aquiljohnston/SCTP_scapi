bc94fb175bb3873f4ca7e1aa4c0f12a17ed67f3f
aquiljohston <aquiljohnston3@gmail.com>
2016-08-26 11:03:15 -0400

    Added model for assigned work queue and updated route to populate assigned table.

--- SCAPI/scapi/modules/v1/modules/pge/controllers/DispatchController.php
+++ SCAPI/scapi/modules/v1/modules/pge/controllers/DispatchController.php
@@ -10,6 +10,7 @@ use app\authentication\TokenAuth;
 use app\modules\v1\models\BaseActiveRecord;
 use app\modules\v1\controllers\BaseActiveController;
 use app\modules\v1\modules\pge\models\WebManagementDispatch;
+use app\modules\v1\modules\pge\models\WebManagementAssignedWorkQueue;
 use app\modules\v1\modules\pge\models\WebManagementUsers;
 use app\modules\v1\modules\pge\models\AssignedWorkQueue;
 use yii\web\ForbiddenHttpException;
@@ -84,7 +85,6 @@ class DispatchController extends Controller
 				['like', 'ComplianceDueDate', $filter],
 				['like', 'SAP Released', $filter],
 				['like', 'Assigned', $filter],
-				['like', 'ComplianceYearMonth', $filter],
 				]);
 			}
 			
@@ -110,101 +110,84 @@ class DispatchController extends Controller
 	{
 		try
 		{
-			$asset1 = [];
-			$asset1["Division"] = "Diablo";
-			$asset1["Work Center"] = "Izual";
-			$asset1["Survey Type"] = "3 YR";
-			$asset1["Map/Plat"] = "161-30-5-C";
-			$asset1["Notification ID"] = "12345678";
-			$asset1["Surveyor"] = "Doe, John";
-			$asset1["Employee Type"] = "Employee";
-			$asset1["Compliance Date"] = "08/10/2016";
-			$asset1["Status"] = "Accepted";
-			$asset1["Dispatch Method"] = "Dispatched";
+			$assetQuery = WebManagementAssignedWorkQueue::find();
 			
-			$asset2 = [];
-			$asset2["Division"] = "Malthael";
-			$asset2["Work Center"] = "Urzael";
-			$asset2["Survey Type"] = "1 YR";
-			$asset2["Map/Plat"] = "120-31-6-F";
-			$asset2["Notification ID"] = "13572468";
-			$asset2["Surveyor"] = "Doe, Jane";
-			$asset2["Employee Type"] = "Employee";
-			$asset2["Compliance Date"] = "08/11/2016";
-			$asset2["Status"] = "Dispatched";
-			$asset2["Dispatch Method"] = "Self Dispatched";
-			
-			$asset3 = [];
-			$asset3["Division"] = "Azmodan";
-			$asset3["Work Center"] = "Cydaea";
-			$asset3["Survey Type"] = "5 YR";
-			$asset3["Map/Plat"] = "141-31-3-C";
-			$asset3["Notification ID"] = "24681357";
-			$asset3["Surveyor"] = "Smith, Bob";
-			$asset3["Employee Type"] = "Employee";
-			$asset3["Compliance Date"] = "08/09/2016";
-			$asset3["Status"] = "In Progress";
-			$asset3["Dispatch Method"] = "Ad Hoc";
-			
-			$asset4 = [];
-			$asset4["Division"] = "Belial";
-			$asset4["Work Center"] = "Zoltun Kulle";
-			$asset4["Survey Type"] = "Semi Annual";
-			$asset4["Map/Plat"] = "133-34-4-C";
-			$asset4["Notification ID"] = "12563478";
-			$asset4["Surveyor"] = "Milstone, Fred";
-			$asset4["Employee Type"] = "Employee";
-			$asset4["Compliance Date"] = "08/12/2016";
-			$asset4["Status"] = "Accepted";
-			$asset4["Dispatch Method"] = "Dispatched";
-			
-			$assets = [];
-			$assets[] = $asset1;
-			$assets[] = $asset2;
-			$assets[] = $asset3;
-			$assets[] = $asset4;
-			$assetCount = count($assets);
-			
-			$data = [];
-			
-			//filter assets
-			for($i = 0; $i < $assetCount; $i++)
+			if($division != null)
 			{
-				if($filter == null || stripos($assets[$i]["Division"], $filter) !== false || stripos($assets[$i]["Work Center"], $filter) !== false
-				|| stripos($assets[$i]["Survey Type"], $filter) !== false || stripos($assets[$i]["Map/Plat"], $filter) !== false
-				|| stripos($assets[$i]["Notification ID"], $filter) !== false || stripos($assets[$i]["Surveyor"], $filter) !== false
-				|| stripos($assets[$i]["Employee Type"], $filter) !== false || stripos($assets[$i]["Compliance Date"], $filter) !== false
-				|| stripos($assets[$i]["Status"], $filter) !== false || stripos($assets[$i]["Dispatch Method"], $filter) !== false)
-				{
-					if($division == null || $assets[$i]["Division"] == $division)
-					{
-						if($workCenter == null || $assets[$i]["Work Center"] == $workCenter)
-						{
-							if($mapPlat == null || $assets[$i]["Map/Plat"] == $mapPlat)
-							{
-								if($status == null || $assets[$i]["Status"] == $status)
-								{
-									if($dispatchMethod == null || $assets[$i]["Dispatch Method"] == $dispatchMethod)
-									{
-										$dateArray = explode("/", $assets[$i]["Compliance Date"]);
-										$month = $dateArray[0];
-										//compliance month
-										if($complianceMonth == null || $month == $complianceMonth)
-										{
-											$data[] = $assets[$i];
-										}
-									}
-								}
-							}
-						}
-					}
-				}
+				$assetQuery->andWhere(['Division'=>$division]);
 			}
+			
+			if($workCenter != null)
+			{
+				$assetQuery->andWhere(['WorkCenter'=>$workCenter]);
+			}
+			
+			if($mapPlat != null)
+			{
+				$assetQuery->andWhere(['MapPlat'=>$mapPlat]);
+			}
+			
+			if($status != null)
+			{
+				$assetQuery->andWhere(['Status'=>$status]);
+			}
+			
+			if($complianceMonth != null)
+			{
+				$assetQuery->andWhere(['ComplianceYearMonth'=>$complianceMonth]);
+			}
+			
+			if($filter != null)
+			{
+				$assetQuery->andFilterWhere([
+				'or',
+				['like', 'Division', $filter],
+				['like', 'WorkCenter', $filter],
+				['like', 'SurveyType', $filter],
+				['like', 'MapPlat', $filter],
+				['like', 'Notification ID', $filter],
+				['like', 'Compliance Date', $filter],
+				['like', 'Surveyor', $filter],
+				['like', 'Employee Type', $filter],
+				['like', 'Status', $filter],
+				['like', 'Dispatch Method', $filter],
+				['like', 'ComplianceYearMonth', $filter],
+				]);
+			}
+			
+			$assets = $assetQuery->all();
 			
 			//send response
 			$response = Yii::$app->response;
 			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
+			$response->data = $assets;
+			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+            throw new \yii\web\HttpException(400);
+        }
+	}
+	
+	public function actionGetAssignedWorkQueues()
+	{
+		try
+		{
+			WebManagementAssignedWorkQueue::setClient('CometTracker');
+			$UID = BaseActiveController::getUserFromToken()->UserUID;
+			
+			$assignedWork = WebManagementAssignedWorkQueue::find()
+				->where(['UserUID'=>$UID])
+				->all();
+			
+			//send response
+			$response = Yii::$app->response;
+			$response->format = Response::FORMAT_JSON;
+			$response->data = $assignedWork;
 			return $response;
 		}
         catch(ForbiddenHttpException $e)
@@ -253,8 +236,8 @@ class DispatchController extends Controller
 	
 	public function actionDispatch()
 	{
-		try
-		{
+		// try
+		// {
 			$post = file_get_contents("php://input");
 			$data = json_decode($post, true);
 			$responseData = [];
@@ -281,10 +264,6 @@ class DispatchController extends Controller
 				{
 					$responseData[] = $assignment;
 				}
-				// else
-				// {
-					// print_r($assignment->getErrors());
-				// }
 			}
 			
 			//send response
@@ -292,14 +271,14 @@ class DispatchController extends Controller
 			$response->format = Response::FORMAT_JSON;
 			$response->data = $responseData;
 			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
+		// }
+        // catch(ForbiddenHttpException $e)
+        // {
+            // throw new ForbiddenHttpException;
+        // }
+        // catch(\Exception $e)
+        // {
+            // throw new \yii\web\HttpException(400);
+        // }
 	}
 }
---
+++ SCAPI/scapi/modules/v1/modules/pge/models/WebManagementAssignedWorkQueue.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\modules\v1\modules\pge\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "vWebManagementAssignedWorkQueue".
+ *
+ * @property string $Division
+ * @property string $WorkCenter
+ * @property string $SurveyType
+ * @property string $MapPlat
+ * @property string $Notification ID
+ * @property string $Surveyor
+ * @property string $Employee Type
+ * @property string $Compliance Date
+ * @property string $Status
+ * @property string $Dispatch Method
+ * @property string $IR UID
+ * @property string $Assigned Work Queue UID
+ * @property string $UserUID
+ * @property string $ComplianceYearMonth
+ */
+class WebManagementAssignedWorkQueue extends \app\modules\v1\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vWebManagementAssignedWorkQueue';
+    }
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('pgeDevDb');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['Division', 'WorkCenter', 'SurveyType', 'MapPlat', 'Notification ID', 'Surveyor', 'Employee Type', 'Status', 'Dispatch Method', 'IR UID', 'Assigned Work Queue UID', 'UserUID', 'ComplianceYearMonth'], 'string'],
+            [['Compliance Date'], 'safe'],
+            [['IR UID'], 'required']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'Division' => 'Division',
+            'WorkCenter' => 'Work Center',
+            'SurveyType' => 'Survey Type',
+            'MapPlat' => 'Map Plat',
+            'Notification ID' => 'Notification  ID',
+            'Surveyor' => 'Surveyor',
+            'Employee Type' => 'Employee  Type',
+            'Compliance Date' => 'Compliance  Date',
+            'Status' => 'Status',
+            'Dispatch Method' => 'Dispatch  Method',
+            'IR UID' => 'Ir  Uid',
+            'Assigned Work Queue UID' => 'Assigned  Work  Queue  Uid',
+            'UserUID' => 'User Uid',
+            'ComplianceYearMonth' => 'Compliance Year Month',
+        ];
+    }
+}
