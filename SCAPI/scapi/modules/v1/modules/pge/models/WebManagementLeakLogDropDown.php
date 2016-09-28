b19c47e6245a661ebccd875bf29ab05efdf5403f
Andrew Scott <ascott@gmail.com>
2016-09-28 16:15:20 -0700

    Added database calls for leak log management

---
+++ SCAPI/scapi/modules/v1/modules/pge/models/WebManagementLeakLogDropDown.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\modules\v1\modules\pge\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "vWebManagementDropDownUserWorkCenter".
+ *
+ * @property string $WorkCenter
+ * @property string $WorkCenterUID
+ */
+class WebManagementLeakLogDropDown extends \app\modules\v1\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vWebManagementLeakLogDropDown';
+    }
+//
+//    /**
+//     * @inheritdoc
+//     */
+//    public function rules()
+//    {
+//        return [
+//            [['WorkCenter', 'WorkCenterUID'], 'string']
+//        ];
+//    }
+//
+//    /**
+//     * @inheritdoc
+//     */
+//    public function attributeLabels()
+//    {
+//        return [
+//            'WorkCenter' => 'Work Center',
+//            'WorkCenterUID' => 'Work Center Uid',
+//        ];
+//    }
+}
