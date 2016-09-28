234fb209aa34c7c9a4c7a0314bf53eeed49dc7dd
Michael Davis <mdavis@southerncrossinc.com>
2016-09-28 15:33:04 -0400

    Implemented functions to parse wind speed and equipment calibration activities.

--- SCAPI/scapi/modules/v1/modules/pge/controllers/EquipmentController.php
+++ SCAPI/scapi/modules/v1/modules/pge/controllers/EquipmentController.php
@@ -9,6 +9,7 @@ use yii\filters\VerbFilter;
 use app\authentication\TokenAuth;
 use app\modules\v1\controllers\BaseActiveController;
 use app\modules\v1\modules\pge\models\TabletEquipment;
+use app\modules\v1\modules\pge\models\InspectionsEquipment;
 use app\modules\v1\models\BaseActiveRecord;
 use yii\web\ForbiddenHttpException;
 use yii\web\BadRequestHttpException;
@@ -52,6 +53,55 @@ class EquipmentController extends Controller
 			$response->format = Response::FORMAT_JSON;
 			$response->data = $data;
 			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+            throw new \yii\web\HttpException(400);
+        }
+	}
+	
+		//Parses Wind Speed Array from the activity json and stores data into the table.
+	public static function calibrationCreate($equipmentCalibrationArray, $client, $userUID)
+	{
+		try
+		{
+			//set db target
+			BaseActiveRecord::setClient($client);
+			
+			$savedData = [];
+
+			//Indications
+			if($equipmentCalibrationArray != null)
+			{
+				//loop wind speed entries
+				$equipmentCalibrationCount = (count($equipmentCalibrationArray));
+				for ($i = 0; $i < $equipmentCalibrationCount; $i++)
+				{
+					//new WindSpeed model
+					$equipmentCalibration = new InspectionsEquipment();
+					//pass data to model
+					$equipmentCalibration->attributes = $equipmentCalibrationArray[$i];
+					//additional fields
+					$equipmentCalibration->CreatedUserUID = $userUID;
+					$equipmentCalibration->ModifiedUserUID = $userUID;
+					
+					//save model
+					if($equipmentCalibration->save())
+					{
+						//add to response array
+						$savedData[] = $equipmentCalibration;
+					}
+					else
+					{
+						$savedData[] = 'Failed to Save Equipment Calibration Record';
+					}
+				}
+			}
+			return $savedData;		
 		}
         catch(ForbiddenHttpException $e)
         {
