3b2fa7980f48c3226457577fe078a39923da4080
aquiljohston <aquiljohnston3@gmail.com>
2016-09-30 14:15:10 -0400

    Implemented Breadcrumb route to save breadcrumbs to CT and PGE db.

--- SCAPI/scapi/modules/v1/models/Breadcrumb.php
+++ SCAPI/scapi/modules/v1/models/Breadcrumb.php
@@ -7,40 +7,36 @@ use Yii;
 /**
  * This is the model class for table "BreadcrumbTb".
  *
- * @property integer $BreadcrumbOBJECTID
- * @property integer $BreadcrumbRecID
- * @property integer $BreadcrumbPersonID
- * @property integer $BreadcrumbLoginID
- * @property integer $BreadcrumbAddressID
- * @property string $BreadcrumbTimetag
- * @property integer $BreadcrumbNumSats
- * @property string $BreadcrumbLatitude
- * @property string $BreadcrumbLongitude
- * @property string $BreadcrumbPosSrc
- * @property integer $BreadcrumbSpeed
- * @property integer $BreadcrumbHeading
- * @property string $BreadcrumbDirection
- * @property integer $BreadcrumbSeqValue
+ * @property integer $BreadcrumbID
+ * @property string $BreadcrumbUID
+ * @property string $BreadcrumbActivityUID
+ * @property string $BreadcrumbSourceID
+ * @property double $BreadcrumbLatitude
+ * @property double $BreadcrumbLongitude
+ * @property double $BreadcrumbSpeed
+ * @property string $BreadcrumbHeading
  * @property string $BreadcrumbDeviceID
- * @property string $BreadcrumbLocalTimeTag
- * @property string $BreadcrumbGpsAccuracy
- * @property integer $BreadcrumbAssociatedPhoneData
- * @property string $BreadcrumbSpatialPosition
- * @property integer $BreadcrumbViolationZoneID
+ * @property string $BreadcrumbGPSAccuracy
+ * @property string $BreadcrumbGPSType
+ * @property string $BreadcrumbGPSSentence
+ * @property string $BreadcrumbGPSTime
+ * @property string $BreadcrumbShape
+ * @property string $BreadcrumbWorkQueueFilter
+ * @property string $BreadcrumbActivityType
  * @property integer $BreadcrumbTrackingGroupID
- * @property string $BreadcrumbLast_
- * @property string $BreadcrumbFirst_
- * @property string $BreadcrumbGlobalID
- * @property integer $BreadcrumbMap_ID_Num
- * @property integer $BreadcrumbShape
- * @property string $BreadcrumbArchiveFlag
+ * @property integer $BreadcrumbSatellites
+ * @property double $BreadcrumbAltitude
+ * @property integer $BreadcrumbMapPlat
+ * @property integer $BreadcrumbArchiveFlag
  * @property string $BreadcrumbComments
- * @property string $BreadcrumbCreatedBy
+ * @property string $BreadcrumbCreatedUserUID
+ * @property string $BreadcrumbSrcDTLT
+ * @property string $BreadcrumbSrvDTLT
+ * @property string $BreadcrumbSrvDTLTOffset
  * @property string $BreadcrumbCreatedDate
- * @property string $BreadcrumbModifiedBy
- * @property string $BreadcrumbModifiedDate
+ * @property double $BreadcrumbBatteryLevel
  */
-class Breadcrumb extends BaseActiveRecord
+class Breadcrumb extends \app\modules\v1\models\BaseActiveRecord
 {
     /**
      * @inheritdoc
@@ -56,12 +52,10 @@ class Breadcrumb extends BaseActiveRecord
     public function rules()
     {
         return [
-            [['BreadcrumbOBJECTID', 'BreadcrumbTimetag', 'BreadcrumbNumSats', 'BreadcrumbLatitude', 'BreadcrumbLongitude', 'BreadcrumbPosSrc', 'BreadcrumbAssociatedPhoneData', 'BreadcrumbLast_', 'BreadcrumbFirst_', 'BreadcrumbGlobalID'], 'required'],
-            [['BreadcrumbOBJECTID', 'BreadcrumbRecID', 'BreadcrumbPersonID', 'BreadcrumbLoginID', 'BreadcrumbAddressID', 'BreadcrumbNumSats', 'BreadcrumbSpeed', 'BreadcrumbHeading', 'BreadcrumbSeqValue', 'BreadcrumbAssociatedPhoneData', 'BreadcrumbViolationZoneID', 'BreadcrumbTrackingGroupID', 'BreadcrumbMap_ID_Num', 'BreadcrumbShape'], 'integer'],
-            [['BreadcrumbTimetag', 'BreadcrumbLocalTimeTag'], 'safe'],
-            [['BreadcrumbLatitude', 'BreadcrumbLongitude', 'BreadcrumbGpsAccuracy'], 'number'],
-            [['BreadcrumbPosSrc', 'BreadcrumbDirection', 'BreadcrumbDeviceID', 'BreadcrumbSpatialPosition', 'BreadcrumbLast_', 'BreadcrumbFirst_', 'BreadcrumbGlobalID', 'BreadcrumbArchiveFlag', 'BreadcrumbComments',
-				'BreadcrumbCreatedBy', 'BreadcrumbCreatedDate', 'BreadcrumbModifiedBy', 'BreadcrumbModifiedDate'], 'string']
+            [['BreadcrumbUID', 'BreadcrumbActivityUID', 'BreadcrumbSourceID', 'BreadcrumbHeading', 'BreadcrumbDeviceID', 'BreadcrumbGPSAccuracy', 'BreadcrumbGPSType', 'BreadcrumbGPSSentence', 'BreadcrumbShape', 'BreadcrumbWorkQueueFilter', 'BreadcrumbActivityType', 'BreadcrumbComments', 'BreadcrumbCreatedUserUID'], 'string'],
+            [['BreadcrumbLatitude', 'BreadcrumbLongitude', 'BreadcrumbSpeed', 'BreadcrumbAltitude', 'BreadcrumbBatteryLevel'], 'number'],
+            [['BreadcrumbGPSTime', 'BreadcrumbSrcDTLT', 'BreadcrumbSrvDTLT', 'BreadcrumbSrvDTLTOffset', 'BreadcrumbCreatedDate'], 'safe'],
+            [['BreadcrumbTrackingGroupID', 'BreadcrumbSatellites', 'BreadcrumbMapPlat', 'BreadcrumbArchiveFlag'], 'integer']
         ];
     }
 
@@ -71,39 +65,34 @@ class Breadcrumb extends BaseActiveRecord
     public function attributeLabels()
     {
         return [
-            'BreadcrumbOBJECTID' => 'Breadcrumb Objectid',
-            'BreadcrumbRecID' => 'Breadcrumb Rec ID',
-            'BreadcrumbPersonID' => 'Breadcrumb Person ID',
-            'BreadcrumbLoginID' => 'Breadcrumb Login ID',
-            'BreadcrumbAddressID' => 'Breadcrumb Address ID',
-            'BreadcrumbTimetag' => 'Breadcrumb Timetag',
-            'BreadcrumbNumSats' => 'Breadcrumb Num Sats',
+            'BreadcrumbID' => 'Breadcrumb ID',
+            'BreadcrumbUID' => 'Breadcrumb Uid',
+            'BreadcrumbActivityUID' => 'Breadcrumb Activity Uid',
+            'BreadcrumbSourceID' => 'Breadcrumb Source ID',
             'BreadcrumbLatitude' => 'Breadcrumb Latitude',
             'BreadcrumbLongitude' => 'Breadcrumb Longitude',
-            'BreadcrumbPosSrc' => 'Breadcrumb Pos Src',
             'BreadcrumbSpeed' => 'Breadcrumb Speed',
             'BreadcrumbHeading' => 'Breadcrumb Heading',
-            'BreadcrumbDirection' => 'Breadcrumb Direction',
-            'BreadcrumbSeqValue' => 'Breadcrumb Seq Value',
             'BreadcrumbDeviceID' => 'Breadcrumb Device ID',
-            'BreadcrumbLocalTimeTag' => 'Breadcrumb Local Time Tag',
-            'BreadcrumbGpsAccuracy' => 'Breadcrumb Gps Accuracy',
-            'BreadcrumbAssociatedPhoneData' => 'Breadcrumb Associated Phone Data',
-            'BreadcrumbSpatialPosition' => 'Breadcrumb Spatial Position',
-            'BreadcrumbViolationZoneID' => 'Breadcrumb Violation Zone ID',
-            'BreadcrumbTrackingGroupID' => 'Breadcrumb Tracking Group ID',
-            'BreadcrumbLast_' => 'Breadcrumb Last',
-            'BreadcrumbFirst_' => 'Breadcrumb First',
-            'BreadcrumbGlobalID' => 'Breadcrumb Global ID',
-            'BreadcrumbMap_ID_Num' => 'Breadcrumb Map  Id  Num',
+            'BreadcrumbGPSAccuracy' => 'Breadcrumb Gpsaccuracy',
+            'BreadcrumbGPSType' => 'Breadcrumb Gpstype',
+            'BreadcrumbGPSSentence' => 'Breadcrumb Gpssentence',
+            'BreadcrumbGPSTime' => 'Breadcrumb Gpstime',
             'BreadcrumbShape' => 'Breadcrumb Shape',
-			'BreadcrumbArchiveFlag' => 'Breadcrumb Archive Flag',
-			'BreadcrumbComments' => 'Breadcrumb Comments',
-			'BreadcrumbCreatedBy' => 'Breadcrumb Created By',
-			'BreadcrumbCreatedDate' => 'Breadcrumb Created Date',
-			'BreadcrumbModifiedBy' => 'Breadcrumb Modified By',
-			'BreadcrumbModifiedDate' => 'Breadcrumb Modified Date',
-			
+            'BreadcrumbWorkQueueFilter' => 'Breadcrumb Work Queue Filter',
+            'BreadcrumbActivityType' => 'Breadcrumb Activity Type',
+            'BreadcrumbTrackingGroupID' => 'Breadcrumb Tracking Group ID',
+            'BreadcrumbSatellites' => 'Breadcrumb Satellites',
+            'BreadcrumbAltitude' => 'Breadcrumb Altitude',
+            'BreadcrumbMapPlat' => 'Breadcrumb Map Plat',
+            'BreadcrumbArchiveFlag' => 'Breadcrumb Archive Flag',
+            'BreadcrumbComments' => 'Breadcrumb Comments',
+            'BreadcrumbCreatedUserUID' => 'Breadcrumb Created User Uid',
+            'BreadcrumbSrcDTLT' => 'Breadcrumb Src Dtlt',
+            'BreadcrumbSrvDTLT' => 'Breadcrumb Srv Dtlt',
+            'BreadcrumbSrvDTLTOffset' => 'Breadcrumb Srv Dtltoffset',
+            'BreadcrumbCreatedDate' => 'Breadcrumb Created Date',
+            'BreadcrumbBatteryLevel' => 'Breadcrumb Battery Level',
         ];
     }
 }
