e13ff4a9aa4281b1afad15e9382c5526255fd625
aquiljohston <aquiljohnston3@gmail.com>
2016-10-12 10:18:45 -0400

    Added creation of Inspection Services placeholders to master leak log create.

--- SCAPI/scapi/modules/v1/modules/pge/controllers/MasterLeakLogController.php
+++ SCAPI/scapi/modules/v1/modules/pge/controllers/MasterLeakLogController.php
@@ -10,6 +10,7 @@ use app\authentication\TokenAuth;
 use app\modules\v1\models\BaseActiveRecord;
 use app\modules\v1\controllers\BaseActiveController;
 use app\modules\v1\modules\pge\models\MasterLeakLog;
+use app\modules\v1\modules\pge\models\InspectionService;
 use yii\web\ForbiddenHttpException;
 use yii\web\BadRequestHttpException;
 use yii\db\Connection;
@@ -36,6 +37,8 @@ class MasterLeakLogController extends Controller
 		return $behaviors;	
 	}
 	
+	//takes in an array of new master leak logs and an array of equipment, creates new records for each log 
+	//and new place holder inspection service records for each log equipment pair.
 	public function actionCreate()
 	{
 		//get UID of user making request
@@ -48,23 +51,53 @@ class MasterLeakLogController extends Controller
 		$put = file_get_contents("php://input");
 		$data = json_decode($put, true);
 		
-		$count = count($data['MasterLeakLog']);
+		$logArray = $data['MasterLeakLog']['Logs'];
+		$equipmentArray = $data['MasterLeakLog']['Equipment'];
+		
+		$logCount = count($logArray);
+		$equipmentCount = count($equipmentArray);
 		$responseData = [];
 		
-		for($i = 0; $i < $count; $i++)
+		for($i = 0; $i < $logCount; $i++)
 		{
 			$masterLeakLog = new MasterLeakLog();
-			$masterLeakLog->attributes = $data['MasterLeakLog'][$i];
+			$masterLeakLog->attributes = $logArray[$i];
 			$masterLeakLog->CreatedUserUID = $UserUID;
 			$masterLeakLog->ModifiedUserUID = $UserUID;
 			
 			if($masterLeakLog->save())
 			{
-				$responseData[] = ['MasterLeakLogUID'=>$data['MasterLeakLog'][$i]['MasterLeakLogUID'], 'Success'=>1];
+				$services = [];
+				for($j = 0; $j < $equipmentCount; $j++)
+				{
+					$inspectionService = new InspectionService();
+					$inspectionService->attributes = $logArray[$i];
+					$inspectionService->InspectionServicesUID = BaseActiveController::generateUID('InspectionService', 'API');
+					$inspectionService->InspectionRequestUID = $logArray[$i]['InspectionRequestLogUID'];
+					$inspectionService->InspectionEquipmentUID = $equipmentArray[$j]['InspectionEquipmentUID'];
+					$inspectionService->CreatedUserUID = $UserUID;
+					$inspectionService->ModifiedUserUID = $UserUID;
+					$inspectionService->PlaceHolderFlag = 1;
+					
+					if ($inspectionService->save())
+					{
+						$services[] = ['InspectionServiceUID' => $inspectionService->InspectionServicesUID,
+						'InspectionEquipmentUID' => $inspectionService->InspectionEquipmentUID,
+						'Success' => 1];
+					}
+					else
+					{
+						$services[] = ['InspectionServiceUID' => $inspectionService->InspectionServicesUID,
+							'InspectionEquipmentUID' => $inspectionService->InspectionEquipmentUID,
+							'Success' => 0];
+					}
+				}
+				
+				$responseData[] = ['MasterLeakLogUID'=>$logArray[$i]['MasterLeakLogUID'], 'Success'=>1, 'Services' => $services];
 			}
 			else
 			{
-				$responseData[] = ['MasterLeakLogUID'=>$data['MasterLeakLog'][$i]['MasterLeakLogUID'], 'Success'=>0];
+				$responseData[] = ['MasterLeakLogUID'=>$logArray[$i]['MasterLeakLogUID'], 'Success'=>0, 'Services' => $services];
 			}		
 		}
 		//send response
