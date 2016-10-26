7719f27304641985dbc36b9d130c95996dcab155
jason <jason.peterson@intellitect.com>
2016-10-26 16:04:19 -0700

    Add spWebManagementStampSubmit and spWebManagmentMasterLeakLogSubmit stored procedures.

---
+++ SCAPI/database/vCAT_PGE_GIS/vCAT_PGE_GIS/dbo/Stored Procedures/spWebManagementMapStampSubmit.sql
@@ -0,0 +1,142 @@
+﻿Create Procedure spWebManagementMapStampSubmit
+(
+@InspectionRequestUID varchar(100)
+,@SubmittedUID varchar(100)
+)
+AS
+Declare @InProgressStatusType varchar(200) = 'In Progress'
+	,@SubmitPendingStatusType varchar(200) = 'Submit/Pending'
+	--,@ApprovedNotSubmitted varchar(200) = 'ApprovedNotSubmitted'
+	,@Revision int
+	,@ReturnVal bit = 0
+	
+	IF (Select Count(*)
+				from [dbo].[tInspectionService]
+				where InspectionRequestUID = @InspectionRequestUID 
+					and ActiveFlag = 1 
+					and StatusType in (@InProgressStatusType)) = 0
+		
+	
+	BEGIN
+	
+		Select @Revision = Count(*) From tInspectionRequest where InspectionRequestUID = @InspectionRequestUID
+
+		BEGIN TRY
+
+			BEGIN TRANSACTION
+
+				Update tInspectionRequest set ActiveFlag = 0 where InspectionRequestUID = @InspectionRequestUID
+
+				Insert Into tInspectionRequest
+				(
+					InspectionRequestUID,
+					MapGridUID,
+					ProjectID,
+					SourceID,
+					CreatedUserUID,
+					ModifiedUserUID,
+					CreateDTLT,
+					ModifiedDTLT,
+					Comments,
+					RevisionComments,
+					Revision,
+					ActiveFlag,
+					StatusType,
+					PipelineType,
+					SurveyType,
+					LsNtfNo,
+					OrderNo,
+					MapID,
+					Wall,
+					Plat,
+					MWC,
+					FLOC,
+					InspectionFrequencyType,
+					ComplianceDueDate,
+					ScheduledStartDate,
+					ScheduledCompleteDate,
+					ReleaseDate,
+					PrevServ,
+					PrevFtOfMain,
+					ReturnFlag,
+					ReturnComments,
+					FileCount,
+					ApprovedFlag,
+					ApprovedByUserUID,
+					ApprovedDTLT,
+					SubmittedFlag,
+					SubmittedStatusType,
+					SubmittedUserUID,
+					SubmittedDTLT,
+					ReturnedFlag,
+					ReturnedFromPGEStatusType,
+					RetrunedFromPGEDTLT,
+					CompletedFlag,
+					CompletedDTLT,
+					InspectionType,
+					ActualStartDate
+				)
+				Select
+					InspectionRequestUID,
+					MapGridUID,
+					ProjectID,
+					SourceID,
+					CreatedUserUID,
+					ModifiedUserUID,
+					CreateDTLT,
+					ModifiedDTLT,
+					Comments,
+					RevisionComments,
+					@Revision, -- Revision,
+					1, --ActiveFlag,
+					@SubmitPendingStatusType, --StatusType,
+					PipelineType,
+					SurveyType,
+					LsNtfNo,
+					OrderNo,
+					MapID,
+					Wall,
+					Plat,
+					MWC,
+					FLOC,
+					InspectionFrequencyType,
+					ComplianceDueDate,
+					ScheduledStartDate,
+					ScheduledCompleteDate,
+					ReleaseDate,
+					PrevServ,
+					PrevFtOfMain,
+					ReturnFlag,
+					ReturnComments,
+					FileCount,
+					ApprovedFlag,
+					ApprovedByUserUID,
+					ApprovedDTLT,
+					1, --SubmittedFlag,
+					SubmittedStatusType,
+					@SubmittedUID, --SubmittedUserUID,
+					getdate(), --SubmittedDTLT,
+					ReturnedFlag,
+					ReturnedFromPGEStatusType,
+					RetrunedFromPGEDTLT,
+					CompletedFlag,
+					CompletedDTLT,
+					InspectionType,
+					ActualStartDate
+				From tInspectionRequest where InspectionRequestUID = @InspectionRequestUID and Revision = @Revision - 1
+
+				COMMIT TRANSACTION
+
+				Set @ReturnVal = 1
+
+			END TRY
+			BEGIN CATCH
+
+			ROLLBACK TRANSACTION
+			Set @ReturnVal = 0
+
+			END CATCH
+
+		END
+
+Return @ReturnVal
---
+++ SCAPI/database/vCAT_PGE_GIS/vCAT_PGE_GIS/dbo/Stored Procedures/spWebManagementMasterLeakLogSubmit.sql
@@ -0,0 +1,107 @@
+﻿Create Procedure spWebManagementMasterLeakLogSubmit
+(
+@MasterLeakLogUID varchar(100)
+,@SubmittedUID varchar(100)
+)
+AS
+Declare @ReviewdStatusType varchar(200) = 'Reviewd'
+	,@SubmitPending varchar(200) = 'SubmitPending'
+	,@ApprovedNotSubmitted varchar(200) = 'ApprovedNotSubmitted'
+	,@Revision int
+	,@ReturnVal bit = 0
+	
+	IF (Select Count(*)
+				from [dbo].[tgAssetAddressIndication] 
+				where MasterLeakLogUID = @MasterLeakLogUID 
+					and ActiveFlag = 1 
+					and StatusType in ('In Progress','Pending')) = 0
+		AND (Select Count(*)
+				from tMasterLeakLog
+				where MasterLeakLogUID = @MasterLeakLogUID 
+					and ActiveFlag = 1 
+					and StatusType in (@ApprovedNotSubmitted)) = 1
+	
+	BEGIN
+	
+		Select @Revision = Count(*) From tMasterLeakLog where MasterLeakLogUID = @MasterLeakLogUID
+
+		BEGIN TRY
+
+			BEGIN TRANSACTION
+
+				Update tMasterLeakLog set ActiveFlag = 0 where MasterLeakLogUID = @MasterLeakLogUID
+
+				
+				Insert Into tMasterLeakLog
+				(
+					MasterLeakLogUID,
+					InspectionRequestLogUID,
+					MapGridUID,
+					ServiceDate,
+					ProjectID,
+					SourceID,
+					CreatedUserUID,
+					ModifiedUserUID,
+					Comments,
+					RevisionComments,
+					Revision,
+					ActiveFlag,
+					StatusType,
+					ApprovedFlag,
+					ApprovedByUserUID,
+					ApprovedDTLT,
+					SubmittedFlag,
+					SubmittedStatusType,
+					SubmittedUserUID,
+					SubmittedDTLT,
+					ResponseStatusType,
+					Response,
+					ResponceErrorDescription,
+					ResponseDTLT,
+					CompletedFlag,
+					CompletedDTLT
+				)
+				Select 
+					MasterLeakLogUID,
+					InspectionRequestLogUID,
+					MapGridUID,
+					ServiceDate,
+					ProjectID,
+					SourceID,
+					CreatedUserUID,
+					@SubmittedUID,
+					Comments,
+					RevisionComments,
+					@Revision, -- Revision,
+					1, --ActiveFlag,
+					@SubmitPending, --StatusType,
+					ApprovedFlag,
+					ApprovedByUserUID,
+					ApprovedDTLT,
+					1, --SubmittedFlag,
+					SubmittedStatusType,
+					@SubmittedUID, --SubmittedUserUID,
+					getdate(), --SubmittedDTLT,
+					ResponseStatusType,
+					Response,
+					ResponceErrorDescription,
+					ResponseDTLT,
+					CompletedFlag,
+					CompletedDTLT
+				From tMasterLeakLog where MasterLeakLogUID = @MasterLeakLogUID and revision = @Revision - 1
+
+				COMMIT TRANSACTION
+
+				Set @ReturnVal = 1
+
+			END TRY
+			BEGIN CATCH
+
+			ROLLBACK TRANSACTION
+			Set @ReturnVal = 0
+
+			END CATCH
+
+		END
+
+Return @ReturnVal
