73f714342c4e444232f5186e8ab3c5c485e73d4a
jason <jason.peterson@intellitect.com>
2016-11-04 09:17:58 -0700

    Update database scripts

--- SCAPI/database/vCAT_PGE_GIS/vCAT_PGE_GIS/dbo/Views/vWebManagementMasterLeakLog.sql
+++ SCAPI/database/vCAT_PGE_GIS/vCAT_PGE_GIS/dbo/Views/vWebManagementMasterLeakLog.sql
@@ -10,13 +10,13 @@ AS
 Select 
 
 ISNULL(LeakInfo.LeakCount, 0) [Leaks],
-CASE WHEN mll.StatusType not in ('Not Approved', 'In Progress') THEN 1 else 0 END [Approved],
+CASE WHEN StatusDesc.FieldDisplay not in ('Not Approved', 'In Progress') THEN 1 else 0 END [Approved],
 CASE WHEN ISNULL(LeakInfo.HCA, 0) = 0 THEN 'No' ELSE 'Yes' END [HCA],
-mll.CreateDate [Date], 
+Cast(mll.CreateDate as date) [Date], 
 u.UserLastName + ', ' + u.UserFirstName + ' (' + u.UserLANID + ')' [Surveyor],
 wc.WorkCenter [WorkCenter],
 mg.FuncLocMap + '/' + mg.FuncLocPlat [Map/Plat],
-CASE WHEN IR.InspectionFrequencyType is not null then IR.InspectionFrequencyType else ai.InspectionFreq END [SurveyFreq],
+IR.InspectionFrequencyType [SurveyFreq],
 ISNULL(Services.FeetOfMain, 0) [FeetOfMain], 
 ISNULL(Services.NumofServices, 0) [NumofServices],
 ISNULL(Services.Hours, 0) [Hours], 
@@ -35,7 +35,7 @@ Left Join (Select MasterLeakLogUID, Sum(EstimatedHours) [Hours], Sum(EstimatedFe
 		[Services] on mll.MasterLeakLogUID = Services.MasterLeakLogUID
 Join (select * from UserTb where UserActiveFlag = 1) U
 	on MLL.CreatedUserUID = u.UserUID
-Left Join (select * from tInspectionRequest where ActiveFlag = 1) IR on IR.InspectionRequestUID = MLL.InspectionRequestLogUID
+Join (select * from tInspectionRequest where ActiveFlag = 1) IR on IR.InspectionRequestUID = MLL.InspectionRequestLogUID
 Join (Select * from [dbo].[rgMapGridLog] where ActiveFlag = 1 and StatusType = 'Active') mg
 	on MLL.MapGridUID = mg.MapGridUID
 Join (Select * from rWorkCenter where ActiveFlag = 1)
@@ -55,7 +55,6 @@ Left Join (Select MasterLeakLogUID, Count(*) [phcount]
 				And StatusType <> 'Deleted'
 				And PlaceHolderFlag = 1
 			Group By MasterLeakLogUID) ph on ph.MasterLeakLogUID = mll.MasterLeakLogUID
-Left Join (Select * from tgAssetInspection where ActiveFlag = 1) ai on ai.MasterLeakLogsUID = mll.MasterLeakLogUID
 WHERE Services.MasterLeakLogUID is not null or LeakInfo.MasterLeakLogUID is not null
 
 
