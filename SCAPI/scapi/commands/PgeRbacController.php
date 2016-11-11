104f76cdf5ed668bb8dc8fe5a7de9547918265a9
aquiljohston <aquiljohnston3@gmail.com>
2016-11-11 15:16:55 -0500

    Implemented permissions for dispatch, assigned, map stamp and leak log screens. Based on wireframes.

--- SCAPI/scapi/commands/PgeRbacController.php
+++ SCAPI/scapi/commands/PgeRbacController.php
@@ -51,10 +51,83 @@ class PgeRbacController extends Controller
 		////////////////////////////create permissions////////////////////////////////////////////////////
 		
 		//Dispatch Permissions//
+		//add "viewDispatch" permission
+		$viewDispatch = $auth->createPermission('viewDispatch');
+		$viewDispatch->description = 'View the Dispatch Screen';
+		$auth->add($viewDispatch);
 		//add "dispatch" permission
 		$dispatch = $auth->createPermission('dispatch');
 		$dispatch->description = 'Dispatch IRs to Surveyors in the Field';
 		$auth->add($dispatch);
+		
+		//Assigned Permissions//
+		//add "viewAssigned" permission
+		$viewAssigned = $auth->createPermission('viewAssigned');
+		$viewAssigned->description = 'View the Assigned Screen';
+		$auth->add($viewAssigned);
+		//add "unassign" permission
+		$unassign = $auth->createPermission('unassign');
+		$unassign->description = 'Unassign a Surveyor from an IR';
+		$auth->add($unassign);
+		//add "addSurveyor" permission
+		$addSurveyor = $auth->createPermission('addSurveyor');
+		$addSurveyor->description = 'Add a Surveyor to an IR that has already been assigned';
+		$auth->add($addSurveyor);
+		
+		//Leak Log Permissions//
+		//add "viewLeakLogMgnt" permission
+		$viewLeakLogMgnt = $auth->createPermission('viewLeakLogMgnt');
+		$viewLeakLogMgnt->description = 'View the Leak Log Management screen';
+		$auth->add($viewLeakLogMgnt);
+		//add "viewLeakLogSearch" permission
+		$viewLeakLogSearch = $auth->createPermission('viewLeakLogSearch');
+		$viewLeakLogSearch->description = 'View the Leak Log Search screen';
+		$auth->add($viewLeakLogSearch);
+		//add "viewLeakDetails" permission
+		$viewLeakDetails = $auth->createPermission('viewLeakDetails');
+		$viewLeakDetails->description = 'View the Leak Log Detail screen';
+		$auth->add($viewLeakDetails);
+		//add "submitLeak" permission
+		$submitLeak = $auth->createPermission('submitLeak');
+		$submitLeak->description = 'Submit a Leak to SAP';
+		$auth->add($submitLeak);
+		//add "editLeak" permission
+		$editLeak = $auth->createPermission('editLeak');
+		$editLeak->description = 'Edit a Leak';
+		$auth->add($editLeak);
+		//add "transferFLOC" permission
+		$transferFLOC = $auth->createPermission('transferFLOC');
+		$transferFLOC->description = 'Transfer a Leak to a new Functional Location';
+		$auth->add($transferFLOC);
+		
+		//Map Stamp Permissions//
+		//add "viewMapStampMgnt" permission
+		$viewMapStampMgnt = $auth->createPermission('viewMapStampMgnt');
+		$viewMapStampMgnt->description = 'View Map Stamp Management Screen';
+		$auth->add($viewMapStampMgnt);
+		//add "viewMapStampDetail" permission
+		$viewMapStampDetail = $auth->createPermission('viewMapStampDetail');
+		$viewMapStampDetail->description = 'View Map Stamp Detail Screen';
+		$auth->add($viewMapStampDetail);
+		//add "editMapStamp" permission
+		$editMapStamp = $auth->createPermission('editMapStamp');
+		$editMapStamp->description = 'Edit a Map Stamp Record';
+		$auth->add($editMapStamp);
+		//add "approveMapStamp" permission
+		$approveMapStamp = $auth->createPermission('approveMapStamp');
+		$approveMapStamp->description = 'Approve a Map Stamp Record';
+		$auth->add($approveMapStamp);
+		//add "viewMapStampDetailModal" permission
+		$viewMapStampDetailModal = $auth->createPermission('viewMapStampDetailModal');
+		$viewMapStampDetailModal->description = 'View the detail modal for a Map Stamp';
+		$auth->add($viewMapStampDetailModal);
+		
+		//AOC Permissions//
+		//add "viewAOC" permission
+		$viewAOC = $auth->createPermission('viewAOC');
+		$viewAOC->description = 'View the AOC screen';
+		$auth->add($viewAOC);
+		
 		
 		/////////////////////////// add roles and children //////////////////////////////////////////////
 		
@@ -63,28 +136,54 @@ class PgeRbacController extends Controller
 		$auth->add($surveyorInspector);
 		//add permissions to Surveyor/Inspector
 		
-		//add "BSS/Analyst" role
-		$bssAnalyst = $auth->createRole('BSS/Analyst');
-		$auth->add($bssAnalyst);
-		//add permissions to BSS/Analyst
-		
 		//add "QM" role
 		$qm = $auth->createRole('QM');
 		$auth->add($qm);
+		//add child roles to QM
 		//add permissions to QM
+		$auth->addChild($qm, $viewLeakLogMgnt);
+		$auth->addChild($qm, $viewLeakLogSearch);
+		$auth->addChild($qm, $viewLeakDetails);
+		$auth->addChild($qm, $viewMapStampMgnt);
+		$auth->addChild($qm, $viewMapStampDetail);
+		$auth->addChild($qm, $viewAOC);
+		
+		//add "BSS/Analyst" role
+		$bssAnalyst = $auth->createRole('BSS/Analyst');
+		$auth->add($bssAnalyst);
+		//add child roles to BSS/Analyst
+		$auth->addChild($bssAnalyst, $qm);
+		//add permissions to BSS/Analyst
+		$auth->addChild($bssAnalyst, $viewDispatch);
+		$auth->addChild($bssAnalyst, $viewAssigned);
+		$auth->addChild($bssAnalyst, $viewMapStampDetailModal);
 		
 		//add "Supervisor" role
 		$supervisor = $auth->createRole('Supervisor');
 		$auth->add($supervisor);
+		//add child roles to Supervisor
+		$auth->addChild($supervisor, $bssAnalyst);
 		//add permissions to Supervisor
 		$auth->addChild($supervisor, $dispatch);
+		$auth->addChild($supervisor, $unassign);
+		$auth->addChild($supervisor, $addSurveyor);
+		$auth->addChild($supervisor, $submitLeak);
+		$auth->addChild($supervisor, $editLeak);
+		$auth->addChild($supervisor, $transferFLOC);
+		$auth->addChild($supervisor, $editMapStamp);
+		$auth->addChild($supervisor, $approveMapStamp);
 		
 		//add "Administrator" role
 		$administrator = $auth->createRole('Administrator');
 		$auth->add($administrator);
 		//add child roles to Administrator
-		$auth->addChild($administrator, $supervisor);
+		$auth->addChild($administrator, $bssAnalyst);
 		//add permissions to Administrator
+		$auth->addChild($administrator, $dispatch);
+		$auth->addChild($administrator, $unassign);
+		$auth->addChild($administrator, $addSurveyor);
+		$auth->addChild($administrator, $submitLeak);
+		$auth->addChild($administrator, $transferFLOC);
 		
 		
 		///////////////////////////assign roles to existing users////////////////////////////////////////
