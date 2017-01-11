a8b6de35b4ed79bc5c531ea7b2c189b19735bdc7
Vasile Sumanschi <vasile.sumanschi@intellitect.com>
2017-01-11 22:29:27 +0200

    adds some api actions for tracker map

---
+++ SCAPI/scapi/modules/v1/modules/pge/models/WebManagementTrackerIndications.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\modules\v1\modules\pge\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "﻿vWebManagementTrackerIndications".
+ *
+ */
+class WebManagementTrackerIndications extends \app\modules\v1\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vWebManagementTrackerIndications_Q';
+    }
+}
