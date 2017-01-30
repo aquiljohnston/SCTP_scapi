88ec22a841d5d8144516012578cbd21024553340
aquiljohston <aquiljohnston3@gmail.com>
2017-01-30 14:18:02 -0500

    Added order by to surveyor dropdown routes for AOC and LLM screens. Cleaned up old stub routes in dropdown controller.

--- SCAPI/scapi/modules/v1/controllers/DropdownController.php
+++ SCAPI/scapi/modules/v1/controllers/DropdownController.php
@@ -85,8 +85,8 @@ class DropdownController extends Controller
         $results = [];
         foreach ($values as $value) {
             $results[] = [
-                "id" => $value["Surveyor"],
-                "name" => $value["Surveyor"]
+                'id' => $value['Surveyor'],
+                'name' => $value['Surveyor']
             ];
         }
 
@@ -96,91 +96,6 @@ class DropdownController extends Controller
 
         return $response;
     }
-
-    public function actionGetSurveyorDropdown() {
-        $data["Doe, Jane (janedoe)"] = "Doe, Jane";
-        $data["Doe, John (johndoe)"] = "Doe, John";
-        $data["Milstone, Fred (fred3)"] = "Milstone, Fred";
-        $data["Randalt, Bill (bill2)"] ="Randalt, Bill";
-        $data["Smith, Bob (bob1)"] = "Smith, Bob";
-
-
-        //send response
-        $response = Yii::$app->response;
-        $response->format = Response::FORMAT_JSON;
-        $response->data = $data;
-        return $response;
-    }
-
-    public function actionGetDivisionDropdown()
-    {
-        //TODO RBAC permission check
-        try{
-            //TODO check headers
-
-            //stub data
-            $dropdown = [null => "Select..."];
-            $dropdown["Belial"] = "Belial";
-            $dropdown["Azmodan"] = "Azmodan";
-            $dropdown["Diablo"] = "Diablo";
-            $dropdown["Malthael"] = "Malthael";
-
-            //send response
-            $response = Yii::$app->response;
-            $response ->format = Response::FORMAT_JSON;
-            $response->data = $dropdown;
-            return $response;
-        }
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetWeekDropdown()
-    {
-        //TODO RBAC permission check
-        //try{
-        //TODO check headers
-		try{
-			$dropdown = [];
-			$w1sun = new DateTime("07/24/2016");
-			$w1sun = $w1sun->format('m/d/Y');
-			$w1sat = new DateTime("07/30/2016");
-			$w1sat = $w1sat->format('m/d/Y');
-			$w2sun = new DateTime("07/17/2016");
-			$w2sun = $w2sun->format('m/d/Y');
-			$w2sat = new DateTime("07/23/2016");
-			$w2sat = $w2sat->format('m/d/Y');
-			$w3sun = new DateTime("07/10/2016");
-			$w3sun = $w3sun->format('m/d/Y');
-			$w3sat = new DateTime("07/16/2016");
-			$w3sat = $w3sat->format('m/d/Y');
-			//stub data
-			$dropdown[$w1sun . " - " . $w1sat] = $w1sun . " - " . $w1sat;
-			$dropdown[$w2sun . " - " . $w2sat] = $w2sun . " - " . $w2sat;
-			$dropdown[$w3sun . " - " . $w3sat] = $w3sun . " - " . $w3sat;
-
-			//send response
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $dropdown;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
 
     public function actionGetLeakLogDivisionDropdown()
     {
@@ -197,10 +112,10 @@ class DropdownController extends Controller
                 ->all();
 
             $namePairs = [
-                null => "Select...",
+                null => 'Select...',
             ];
             foreach ($values as $value) {
-                $namePairs[$value["Division"]] = $value["Division"];
+                $namePairs[$value['Division']] = $value['Division'];
             }
 
             $response = Yii::$app ->response;
@@ -254,8 +169,8 @@ class DropdownController extends Controller
                     $surveyType = $value['SurveyFreq'];
                 }
                 $results[] = [
-                    "id" => $surveyType,
-                    "name" => $value["FLOC"]
+                    'id' => $surveyType,
+                    'name' => $value['FLOC']
                 ];
             }
 
@@ -294,8 +209,8 @@ class DropdownController extends Controller
             $results = [];
             foreach ($values as $value) {
                 $results[] = [
-                    "id" => $value["WorkCenter"],
-                    "name" => $value["WorkCenter"]
+                    'id' => $value['WorkCenter'],
+                    'name' => $value['WorkCenter']
                 ];
             }
 
@@ -327,13 +242,14 @@ class DropdownController extends Controller
                 ->select(['Surveyor'])
                 ->where(['WorkCenter' => $workCenter])
                 ->distinct()
+				->orderBy('Surveyor ASC')
                 ->all();
 
             $results = [];
             foreach ($values as $value) {
                 $results[] = [
-                    "id" => $value["Surveyor"],
-                    "name" => $value["Surveyor"]
+                    'id' => $value['Surveyor'],
+                    'name' => $value['Surveyor']
                 ];
             }
 
@@ -387,33 +303,7 @@ class DropdownController extends Controller
             throw new \yii\web\HttpException(400);
         }
     }
-
-    public function actionGetPgeEmployeeTypeDropdown() {
-
-        // TODO: Permissions check
-        try {
-            //TODO: headers and X-Client
-
-            //TODO: Find EmployeeTypes
-            $data = [
-                null => "Select...",
-                "Employee" => "Employee",
-                "Contractor" => "Contractor",
-                "Intern" => "Intern"
-            ];
-
-            $response = Yii::$app->response;
-            $response->format = Response::FORMAT_JSON;
-            $response->data = $data;
-            return $response;
-        }
-        catch (\Exception $e) {
-            throw new BadRequestHttpException;
-        }
-    }
-
-
-
+	
     /*
      * Belongs to LeakLogDetail
      */
@@ -468,8 +358,8 @@ class DropdownController extends Controller
             $results = [];
             foreach ($values as $value) {
                 $results[] = [
-                    "id" => $value["Map/Plat"],
-                    "name" => $value["Map/Plat"]
+                    'id' => $value['Map/Plat'],
+                    'name' => $value['Map/Plat']
                 ];
             }
 
@@ -555,8 +445,8 @@ class DropdownController extends Controller
             $results = [];
             foreach ($values as $value) {
                 $results[] = [
-                    "id" => $value["Surveyor"],
-                    "name" => $value["Surveyor"]
+                    'id' => $value['Surveyor'],
+                    'name' => $value['Surveyor']
                 ];
             }
 
@@ -620,8 +510,8 @@ class DropdownController extends Controller
             $results = [];
             foreach ($values as $value) {
                 $results[] = [
-                    "id" => $value["Date"],
-                    "name" => $value["Date"]
+                    'id' => $value['Date'],
+                    'name' => $value['Date']
                 ];
             }
 
@@ -631,162 +521,6 @@ class DropdownController extends Controller
 
             return $response;
         }
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetReportDropdown() {
-		try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["Report 1"] = "Report 1";
-			$data["Report 2"] = "Report 2";
-			$data["Report 3"] = "Report 3";
-			$data["Report 4"] = "Report 4";
-			$data["Report 5"] = "Report 5";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetSurveyTypeDropdown() {
-        try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["1 YR"] = "1 YR";
-			$data["3 YR"] = "3 YR";
-			$data["5 YR"] = "5 YR";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetComplianceMonthDropdown() {
-		try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["01"] = "January";
-			$data["02"] = "February";
-			$data["03"] = "March";
-			$data["04"] = "April";
-			$data["05"] = "May";
-			$data["06"] = "June";
-			$data["07"] = "July";
-			$data["08"] = "August";
-			$data["09"] = "September";
-			$data["10"] = "October";
-			$data["11"] = "November";
-			$data["12"] = "December";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetStatusDropdown() {
-        try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["Accepted"] = "Accepted";
-			$data["Dispatched"] = "Dispatched";
-			$data["In Progress"] = "In Progress";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-    public function actionGetDispatchMethodDropdown() {
-        try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["Dispatched"] = "Dispatched";
-			$data["Self Dispatched"] = "Self Dispatched";
-			$data["Ad Hoc"] = "Ad Hoc";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
-        catch(ForbiddenHttpException $e)
-        {
-            throw new ForbiddenHttpException;
-        }
-        catch(\Exception $e)
-        {
-            throw new \yii\web\HttpException(400);
-        }
-    }
-
-	public function actionGetDeviceIdDropdown() {
-		try{
-			$data = [];
-
-			$data = [null => "Select..."];
-			$data["12345678"] = "12345678";
-			$data["87654321"] = "87654321";
-			$data["13572468"] = "13572468";
-			$data["24681357"] = "24681357";
-
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
-			$response->data = $data;
-			return $response;
-		}
         catch(ForbiddenHttpException $e)
         {
             throw new ForbiddenHttpException;
