a0d2364d1f0e469c10a882f25c81628c3da3ba39
tzhang <tzhang@speardom.com>
2017-01-31 15:49:35 -0500

    Added WebManagementDropDownAssociatePlanIR model

---
+++ SCAPI/scapi/modules/v1/modules/pge/models/WebManagementDropDownAssociatePlanIR.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\modules\v1\modules\pge\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "﻿vWebManagementTrackerAOC".
+ *
+ */
+class WebManagementDropDownAssociatePlanIR extends \app\modules\v1\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vWebManagementDropDownAssociatePlanIR';
+    }
+}
