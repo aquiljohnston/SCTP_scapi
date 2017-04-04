8f75b608a8d45d723faf59c37aba058d19563aa0
Michael Davis <mdavis@southerncrossinc.com>
2017-04-04 14:27:55 -0400

    Created new v2 login controller with logout by token and altered ctuser to handle both v1 and v2 versions of logout. Removed old code in scuser.

--- SCAPI/scapi/authentication/CTUser.php
+++ SCAPI/scapi/authentication/CTUser.php
@@ -12,61 +12,57 @@ use app\modules\v1\models\BaseActiveRecord;
 
 class CTUser extends User
 {
-	public function clearToken($userID)
+	public function clearTokensByUser($userID)
 	{
 		Auth::setClient(BaseActiveController::urlPrefix());
 		$session = Yii::$app->getSession();
-		Yii::trace('Id of token is '.$userID);
+		Yii::trace('User is '.$userID);
 		$auth = Auth::find()
 			->where(['AuthUserID' => $userID])
 			->one();
-		Yii::trace('Token Found');
 		if ($auth !== null)
 		{
+			Yii::trace('Tokens Found');
+			$auth->delete();
+			Yii::trace('Tokens Removed');
+		}
+		Yii::trace('Token has been cleared');
+	}
+	
+	public function clearTokenByToken($token)
+	{
+		Auth::setClient(BaseActiveController::urlPrefix());
+		$session = Yii::$app->getSession();
+		Yii::trace('Token is '.$token);
+		$auth = Auth::find()
+			->where(['AuthToken' => $token])
+			->one();
+		if ($auth !== null)
+		{
+			Yii::trace('Token Found');
 			$auth->delete();
 			Yii::trace('Token Removed');
 		}
 		Yii::trace('Token has been cleared');
 	}
 	 
-	public function logout($destroySession = true, $userID = null)
+	public function logout($destroySession = true, $userID = null, $token = null)
 	{
-		$this->clearToken($userID);
+		yii::trace('CtUserLogout');
+		if($token != null)
+		{
+			$this->clearTokenByToken($token);
+		}
+		elseif($userID != null)
+		{
+			$this->clearTokensByUser($userID);
+		}
 		parent::logout();
 	}
 	
 	public function checkTimeout($token)
 	{
 		$this->renewAuthStatusWithToken($token);
-	}
-	
-	protected function renewAuthStatus()
-	{
-		$session = Yii::$app->getSession();
-        $id = $session->getHasSessionId() || $session->getIsActive() ? $session->get($this->idParam) : null;
-		Yii::trace('The current user id is'.$id);
-
-        if ($id === null) {
-            $identity = null;
-        } else {
-            /* @var $class IdentityInterface */
-            $class = $this->identityClass;
-            $identity = $class::findIdentity($id);
-        }
-
-        $this->setIdentity($identity);
-
-        if ($identity !== null && ($this->authTimeout !== null || $this->absoluteAuthTimeout !== null)) {
-            $expire = $this->authTimeout !== null ? $session->get($this->authTimeoutParam) : null;
-            $expireAbsolute = $this->absoluteAuthTimeout !== null ? $session->get($this->absoluteAuthTimeoutParam) : null;
-            if ($expire !== null && $expire < time() || $expireAbsolute !== null && $expireAbsolute < time()) {
-				Yii::trace('AuthTimeout has expired and the user will now be logged out');
-				Yii::trace('The current user id now is'.$id);
-				$this->logout(true, $id);
-            } elseif ($this->authTimeout !== null) {
-                $session->set($this->authTimeoutParam, time() + $this->authTimeout);
-            }
-        }
 	}
 		
 	protected function renewAuthStatusWithToken($token)
@@ -90,7 +86,7 @@ class CTUser extends User
 				$auth->AuthModifiedBy = $userID;
 				$auth->update();
 			} else {
-				$this->logout(true, $userID);
+				$this->logout(true, $token);
 			}
 		} else {
 			throw new \yii\web\HttpException(401, 'You are requesting with an invalid credential.');
