f50367bc70ec4cf80dd8bfa7fc2443f0e2972619
Michael Davis <mdavis@southerncrossinc.com>
2017-04-07 17:03:27 -0400

    Implemented new route to call alternative optimization algorithm Simulated annealing. Moved updated ini file, and new tsp with improved SA function.

--- SCAPI/scapi/modules/v2/controllers/RouteController.php
+++ SCAPI/scapi/modules/v2/controllers/RouteController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace app\modules\v2\controllers;
-set_time_limit(600);
+set_time_limit(3600);
 
 use Yii;
 use yii\rest\Controller;
@@ -34,7 +34,7 @@ class RouteController extends Controller
 		return $behaviors;	
 	}
 	
-	public function actionRouteOptimization()
+	public function actionRouteOptimization1()
 	{
 		try
 		{
@@ -87,7 +87,97 @@ class RouteController extends Controller
 			//set execution path
 			chdir($filePath);
 			//execute jar file
-			exec("java -jar TSP.jar $postDataTempName", $output);
+			exec("java -jar TSP.jar $postDataTempName TwoOpt", $output);
+			$responseData = json_decode($output[0]);
+			
+			// $mapCount = count($data['mapGrids']);
+			// for($i; $i < $mapCount; $i++)
+			// {
+				// $mapGrid = $data['mapGrids'][$i];
+				// //TODO query
+				// // $assets = Asset::find()
+					// // ->select(AssetUID, RouteOrder)
+					// // ->where(['MapGrid' => $mapGrid])
+					// // ->orderBy('RouteOrder')
+					// // ->all()
+				
+				// $mapData['MapGrid'] = $mapGrid;
+				// $mapData['assetOrderSequence'] = $assets;
+				
+				// $responseArray['routes'][] = $mapData;
+			// }
+
+			//clean up temp file
+			unlink($postDataTemp);
+			
+			//send response
+			$response->data = $responseData;
+			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+            throw new \yii\web\HttpException(400);
+        }
+	}
+	
+	public function actionRouteOptimization2()
+	{
+		try
+		{
+			//get header and set db path
+			$headers = getallheaders();
+			if(array_key_exists('X-Client', $headers))
+			{
+				BaseActiveRecord::setClient($headers['X-Client']);
+			}
+			
+			//get post data
+			$post = file_get_contents("php://input");			
+			$data = json_decode($post, true);
+			
+			//create response object
+			$response = Yii::$app->response;
+			$response->format = Response::FORMAT_JSON;
+			
+			//check possible data keys
+			if($data['mapGrids'] != null)
+			{
+				//convert post data to a string for java param
+				$paramString = 'mapGrids';
+				$paramString = $paramString . '~' . json_encode($data['mapGrids']);	
+			}
+			elseif($data['routes'] != null)
+			{
+				//convert post data to a string for java param
+				$paramString = 'routes';
+				$paramString = $paramString . '~' . json_encode($data['routes']);	
+			}
+			else
+			{
+				$response->statusCode = 400;
+				$response->data = 'Bad Request - No valid data key present.';
+				return $response;
+			}
+			//file path for jar
+			$filePath = Yii::$app->basePath . "\web\jar";
+			
+			//create temp file for data
+			$postDataTemp = tempnam($filePath, 'TSP');
+			$postDataTempName = basename($postDataTemp);
+			$handle = fopen($postDataTemp, 'w');
+			fwrite($handle, $paramString);
+			fclose($handle);
+			
+			//$responseData = $postDataTempName;
+			
+			//set execution path
+			chdir($filePath);
+			//execute jar file
+			exec("java -jar TSP.jar $postDataTempName SimulatedAnnealing", $output);
 			$responseData = json_decode($output[0]);
 			
 			// $mapCount = count($data['mapGrids']);
