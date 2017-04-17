d7fde5e06b530d2fda94a13b3912b1772bdec5b0
aquiljohston <aquiljohnston3@gmail.com>
2017-04-17 15:11:21 -0400

    Refactored route controller to allow code reuse with internal routeOptimization function

--- SCAPI/scapi/modules/v2/controllers/RouteController.php
+++ SCAPI/scapi/modules/v2/controllers/RouteController.php
@@ -49,80 +49,8 @@ class RouteController extends Controller
 			$post = file_get_contents("php://input");			
 			$data = json_decode($post, true);
 			
-			//create response object
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
+			$response = self::routeOptimization($data, "TwoOpt");
 			
-			//check possible data keys
-			if($data['mapGrids'] != null)
-			{
-				//convert post data to a string for java param
-				$paramString = 'mapGrids';
-				$paramString = $paramString . '~' . json_encode($data['mapGrids']);	
-			}
-			elseif($data['routes'] != null)
-			{
-				//convert post data to a string for java param
-				$paramString = 'routes';
-				$paramString = $paramString . '~' . json_encode($data['routes']);	
-			}
-			else
-			{
-				$response->statusCode = 400;
-				$response->data = 'Bad Request - No valid data key present.';
-				return $response;
-			}
-			
-			//check the isRoundTrip option if value is not present default to true
-			if(array_key_exists('isRoundTrip', $data))
-			{
-				$isRoundTrip = $data['isRoundTrip'];
-			}
-			else
-			{
-				$isRoundTrip = true;
-			}
-			
-			//file path for jar
-			$filePath = Yii::$app->basePath . "\web\jar";
-			
-			//create temp file for data
-			$postDataTemp = tempnam($filePath, 'TSP');
-			$postDataTempName = basename($postDataTemp);
-			$handle = fopen($postDataTemp, 'w');
-			fwrite($handle, $paramString);
-			fclose($handle);
-			
-			//$responseData = $postDataTempName;
-			
-			//set execution path
-			chdir($filePath);
-			//execute jar file
-			exec("java -jar TSP.jar $postDataTempName TwoOpt $isRoundTrip", $output);
-			$responseData = json_decode($output[0]);
-			
-			// $mapCount = count($data['mapGrids']);
-			// for($i; $i < $mapCount; $i++)
-			// {
-				// $mapGrid = $data['mapGrids'][$i];
-				// //TODO query
-				// // $assets = Asset::find()
-					// // ->select(AssetUID, RouteOrder)
-					// // ->where(['MapGrid' => $mapGrid])
-					// // ->orderBy('RouteOrder')
-					// // ->all()
-				
-				// $mapData['MapGrid'] = $mapGrid;
-				// $mapData['assetOrderSequence'] = $assets;
-				
-				// $responseArray['routes'][] = $mapData;
-			// }
-
-			//clean up temp file
-			unlink($postDataTemp);
-			
-			//send response
-			$response->data = $responseData;
 			return $response;
 		}
         catch(ForbiddenHttpException $e)
@@ -150,81 +78,8 @@ class RouteController extends Controller
 			$post = file_get_contents("php://input");			
 			$data = json_decode($post, true);
 			
-			//create response object
-			$response = Yii::$app->response;
-			$response->format = Response::FORMAT_JSON;
+			$response = self::routeOptimization($data, "SimulatedAnnealing");
 			
-			//check possible data keys
-			if($data['mapGrids'] != null)
-			{
-				//convert post data to a string for java param
-				$paramString = 'mapGrids';
-				$paramString = $paramString . '~' . json_encode($data['mapGrids']);	
-			}
-			elseif($data['routes'] != null)
-			{
-				//convert post data to a string for java param
-				$paramString = 'routes';
-				$paramString = $paramString . '~' . json_encode($data['routes']);	
-			}
-			else
-			{
-				$response->statusCode = 400;
-				$response->data = 'Bad Request - No valid data key present.';
-				return $response;
-			}
-			
-			//check the isRoundTrip option if value is not present default to true
-			if(array_key_exists('isRoundTrip', $data))
-			{
-				$isRoundTrip = $data['isRoundTrip'];
-			}
-			else
-			{
-				$isRoundTrip = true;
-			}
-			
-			//file path for jar
-			$filePath = Yii::$app->basePath . "\web\jar";
-			
-			//create temp file for data
-			$postDataTemp = tempnam($filePath, 'TSP');
-			$postDataTempName = basename($postDataTemp);
-			$handle = fopen($postDataTemp, 'w');
-			fwrite($handle, $paramString);
-			fclose($handle);
-			
-			//$responseData = $postDataTempName;
-			
-			//set execution path
-			chdir($filePath);
-			//execute jar file
-			//return "java -jar TSP.jar $postDataTempName SimulatedAnnealing $isRoundTrip";
-			exec("java -jar TSP.jar $postDataTempName SimulatedAnnealing $isRoundTrip", $output);
-			$responseData = json_decode($output[0]);
-			
-			// $mapCount = count($data['mapGrids']);
-			// for($i; $i < $mapCount; $i++)
-			// {
-				// $mapGrid = $data['mapGrids'][$i];
-				// //TODO query
-				// // $assets = Asset::find()
-					// // ->select(AssetUID, RouteOrder)
-					// // ->where(['MapGrid' => $mapGrid])
-					// // ->orderBy('RouteOrder')
-					// // ->all()
-				
-				// $mapData['MapGrid'] = $mapGrid;
-				// $mapData['assetOrderSequence'] = $assets;
-				
-				// $responseArray['routes'][] = $mapData;
-			// }
-
-			//clean up temp file
-			unlink($postDataTemp);
-			
-			//send response
-			$response->data = $responseData;
 			return $response;
 		}
         catch(ForbiddenHttpException $e)
@@ -235,5 +90,85 @@ class RouteController extends Controller
         {
             throw new \yii\web\HttpException(400);
         }
+	}
+	
+	public static function routeOptimization($data, $algorithm)
+	{		
+		//create response object
+		$response = Yii::$app->response;
+		$response->format = Response::FORMAT_JSON;
+		
+		//check possible data keys
+		if($data['mapGrids'] != null)
+		{
+			//convert post data to a string for java param
+			$paramString = 'mapGrids';
+			$paramString = $paramString . '~' . json_encode($data['mapGrids']);	
+		}
+		elseif($data['routes'] != null)
+		{
+			//convert post data to a string for java param
+			$paramString = 'routes';
+			$paramString = $paramString . '~' . json_encode($data['routes']);	
+		}
+		else
+		{
+			$response->statusCode = 400;
+			$response->data = 'Bad Request - No valid data key present.';
+			return $response;
+		}
+		
+		//check the isRoundTrip option if value is not present default to true
+		if(array_key_exists('isRoundTrip', $data))
+		{
+			$isRoundTrip = $data['isRoundTrip'];
+		}
+		else
+		{
+			$isRoundTrip = false;
+		}
+		
+		//file path for jar
+		$filePath = Yii::$app->basePath . "\web\jar";
+		
+		//create temp file for data
+		$postDataTemp = tempnam($filePath, 'TSP');
+		$postDataTempName = basename($postDataTemp);
+		$handle = fopen($postDataTemp, 'w');
+		fwrite($handle, $paramString);
+		fclose($handle);
+		
+		//$responseData = $postDataTempName;
+		
+		//set execution path
+		chdir($filePath);
+		//execute jar file
+		//return "java -jar TSP.jar $postDataTempName $algorithm $isRoundTrip";
+		exec("java -jar TSP.jar $postDataTempName $algorithm $isRoundTrip", $output);
+		$responseData = json_decode($output[0]);
+		
+		// $mapCount = count($data['mapGrids']);
+		// for($i; $i < $mapCount; $i++)
+		// {
+			// $mapGrid = $data['mapGrids'][$i];
+			// //TODO query
+			// // $assets = Asset::find()
+				// // ->select(AssetUID, RouteOrder)
+				// // ->where(['MapGrid' => $mapGrid])
+				// // ->orderBy('RouteOrder')
+				// // ->all()
+			
+			// $mapData['MapGrid'] = $mapGrid;
+			// $mapData['assetOrderSequence'] = $assets;
+			
+			// $responseArray['routes'][] = $mapData;
+		// }
+
+		//clean up temp file
+		unlink($postDataTemp);
+		
+		//send response
+		$response->data = $responseData;
+		return $response;
 	}
 }
