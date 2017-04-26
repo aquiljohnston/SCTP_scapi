8d88f901b64f73a19b6b134d2f5dc46549263e57
Michael Davis <mdavis@southerncrossinc.com>
2017-04-26 17:00:39 -0400

    Moved Windspeed, Taskout, Equipment, AssetAddress, Activity, and Workqueue into v2 and refactored response formats and error logging.

---
+++ SCAPI/scapi/modules/v2/controllers/BaseActiveController.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace app\modules\v2\controllers;
+
+use Yii;
+use app\modules\v2\models\BaseActiveRecord;
+use app\modules\v2\models\SCUser;
+use app\modules\v2\models\TabletDataInsertArchive;
+use app\modules\v2\models\TabletDataInsertBreadcrumbArchive;
+use app\modules\v2\models\TabletJSONDataInsertError;
+use app\authentication\TokenAuth;
+use yii\db\ActiveRecord;
+use yii\rest\ActiveController;
+use yii\web\ForbiddenHttpException;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+use yii\base\ErrorException;
+use yii\db\Exception;
+
+class BaseActiveController extends ActiveController
+{	
+	const DATE_FORMAT = 'Y-m-d H:i:s';
+	private static $IV = 'abcdefghijklmnop';
+	private static $S_KEY = 'sparusholdings12';
+	
+	public function actions()
+	{
+		$actions = parent::actions();
+		unset($actions['create']);
+		return $actions;
+	}
+	
+	/**
+	* sets authenticator for token authentication
+	* sets verb filters for http request
+	* @return an array of behaviors
+	*/
+   public function behaviors()
+    {
+		$behaviors = parent::behaviors();
+		//Implements Token Authentication to check for Auth Token in Json Header
+		$behaviors['authenticator'] =
+		[
+			'class' => TokenAuth::className(),
+		];
+		$behaviors['verbs'] =
+			[
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['delete'],
+					'create' => ['post'],
+					'update' => ['put'],
+					'get-all' => ['get'],
+                ],
+            ];
+		return $behaviors;
+	}
+
+	/**
+	* Creates a record for the sub class's model
+	* @returns json body of the model data
+	* @throws \yii\web\HttpException
+	*/
+	public function actionCreate()
+    {
+		try
+		{
+			//set model class
+			$modelClass = $this->modelClass;
+			
+			//set db target
+			$headers = getallheaders();
+			BaseActiveRecord::setClient(self::urlPrefix());
+			
+			$post = file_get_contents("php://input");
+			$data = json_decode(utf8_decode($post), true);
+
+			$model = new $this->modelClass();
+			$model->attributes = $data;
+			
+			$response = Yii::$app->response;
+			$response ->format = Response::FORMAT_JSON;
+			$response->data = $model;
+			
+			if($model-> save())
+			{
+				$response->setStatusCode(201);
+				return $response;
+			}
+			else
+			{
+				$response->setStatusCode(400);
+				$response->data = "Http:400 Bad Request";
+			}
+		}
+		catch(\Exception $e)  
+		{
+			throw new \yii\web\HttpException(400);
+		}
+    }
+	
+	public function getDate()
+	{
+		return date(BaseActiveController::DATE_FORMAT);
+	}	
+	
+	public static function getUserFromToken($token = null)
+	{
+		if ($token === null) {
+			$token = Yii::$app->request->getAuthUser();
+		}
+		return SCUser::findIdentityByAccessToken($token);
+	}
+
+	
+    public static function inDateRange($day, $startDate, $endDate) {
+        //$day .= " 12:00:00pm";
+        $startDate .= " 12:00:00am";
+        $endDate .= " 11:59:59pm";
+        $dayTS = strtotime($day);
+        return strtotime($startDate) <= $dayTS && $dayTS < strtotime($endDate);
+    }
+	
+	//type: type of data the UID will be associated with such as User, breadcrumb, activty, etc.
+	//source: platform that created the data UID is associated with such as web, mobile, etc.
+	public static function generateUID($type, $source)
+	{
+		//generate random number
+		$random = rand(10000000, 99999999);
+		
+		//get current date time in format YmdHis
+		$date = date("YmdHis");
+		
+		//concat values into string and return the resulting UID
+		return "{$type}_{$random}_{$date}_{$source}";
+	}
+	
+	public static function decrypt($encryptedString)
+	{
+		$decodedString = base64_decode($encryptedString);
+		$decryptedString = openssl_decrypt($decodedString,  'AES-128-CBC', self::$S_KEY, OPENSSL_RAW_DATA, self::$IV);
+		return $decryptedString;
+	}
+	
+	public static function encrypt($string)
+	{
+		$encryptedString = openssl_encrypt($string,  'AES-128-CBC', self::$S_KEY, OPENSSL_RAW_DATA, self::$IV);
+		$encodedString = base64_encode($encryptedString);
+		return $encodedString;
+	}
+	
+	public static function urlPrefix()
+	{
+		$url = explode(".", $_SERVER['SERVER_NAME']);
+		$prefix = $url[0];
+        if(YII_ENV_DEV && (strpos($_SERVER['SERVER_NAME'],'local')!==false
+                ||  $_SERVER['SERVER_NAME'] === '0.0.0.0'
+                || strpos($_SERVER['SERVER_NAME'],'192.168.')===0)
+        )
+        {
+            $prefix = 'apidev';
+        }
+		return $prefix;
+	}
+	
+	// TabletDataInsertArchive;
+	// TabletDataInsertBreadcrumbArchive;
+	// TabletJSONDataInsertError;
+	public static function archiveJson($json, $type, $userUID, $client)
+	{
+		TabletDataInsertArchive::setClient($client);
+		
+		$archiveRecord =  new TabletDataInsertArchive;
+		$archiveRecord->CreatedUserUID = $userUID;
+		$archiveRecord->TransactionType = $type;
+		$archiveRecord->InsertedData = $json;
+		
+		$archiveRecord->save();
+	}
+	
+	public static function archiveBreadcrumbJson($json, $userUID, $client)
+	{
+		TabletDataInsertBreadcrumbArchive::setClient($client);
+		
+		$archiveBreadcrumb = new TabletDataInsertBreadcrumbArchive;
+		$archiveBreadcrumb->UserUID = $userUID;
+		$archiveBreadcrumb->InsertedData = $json;
+		$archiveBreadcrumb->TransactionType = 'Breadcrumb';
+		
+		$archiveBreadcrumb->save();
+	}
+	
+	public static function archiveErrorJson($data, $error, $client, $data2 = null, $data3 = null, $data4 = null, $data5 = null)
+	{
+		TabletJSONDataInsertError::setClient($client);
+		
+		$archiveError = new TabletJSONDataInsertError;
+		$archiveError->InsertedData = $data;
+		$archiveError->InsertedData2 = json_encode($data2);
+		$archiveError->InsertedData3 = json_encode($data3);
+		$archiveError->InsertedData4 = json_encode($data4);
+		$archiveError->InsertedData5 = json_encode($data5);
+		$archiveError->ErrorNumber = $error->getCode();
+		$archiveError->ErrorMessage = $error->getMessage();
+		
+		$archiveError->save();
+	}
+}
---
+++ SCAPI/scapi/modules/v2/modules/pge/models/AssetAddressAOC.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace app\modules\v2\modules\pge\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tgAssetAddressAOC".
+ *
+ * @property integer $AssetAddressAOCID
+ * @property string $AssetAddressAOCUID
+ * @property string $AssetAddressUID
+ * @property string $AssetInspectionUID
+ * @property string $InspectionRequestUID
+ * @property string $MapGridUID
+ * @property integer $ProjectID
+ * @property string $SourceID
+ * @property string $CreatedUserUID
+ * @property string $ModifiedUserUID
+ * @property string $SrcDTLT
+ * @property string $SrvDTLT
+ * @property string $SrvDTLTOffset
+ * @property string $SrcOpenDTLT
+ * @property string $SrcClosedDTLT
+ * @property string $GPSType
+ * @property string $GPSSentence
+ * @property double $Latitude
+ * @property double $Longitude
+ * @property string $SHAPE
+ * @property string $Comments
+ * @property string $RevisionComments
+ * @property integer $Revision
+ * @property integer $ActiveFlag
+ * @property string $StatusType
+ * @property string $AOCType
+ * @property string $AOCReasonType
+ * @property string $AOCOther
+ * @property string $Photo1
+ * @property string $Photo2
+ * @property string $Photo3
+ * @property string $OptionalData1
+ * @property string $OptionalData2
+ * @property string $OptionalData3
+ * @property string $OptionalData4
+ * @property string $OptionalData5
+ * @property integer $ApprovedFlag
+ * @property string $ApprovedByUserUID
+ * @property string $ApprovedDTLT
+ * @property integer $SubmittedFlag
+ * @property string $SubmittedStatusType
+ * @property string $SubmittedUserUID
+ * @property string $SubmittedDTLT
+ * @property string $ResponseStatusType
+ * @property string $Responsecomments
+ * @property string $ResponceErrorComments
+ * @property string $ResponseDTLT
+ * @property integer $CompletedFlag
+ * @property string $CompletedDTLT
+ * @property string $DateFound
+ * @property string $MeterNumber
+ * @property string $GPSSource
+ * @property string $GPSTime
+ * @property integer $FixQuality
+ * @property integer $NumberOfSatellites
+ * @property double $HDOP
+ * @property double $AltitudemetersAboveMeanSeaLevel
+ * @property double $HeightOfGeoid
+ * @property double $TimeSecondsSinceLastDGPS
+ * @property string $ChecksumData
+ * @property double $Bearing
+ * @property double $Speed
+ * @property string $GPSStatus
+ * @property integer $NumberOfGPSAttempts
+ * @property string $MasterLeakLogUID
+ * @property string $ActivityUID
+ */
+class AssetAddressAOC extends \app\modules\v2\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tgAssetAddressAOC';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['AssetAddressAOCUID', 'AssetAddressUID', 'AssetInspectionUID', 'InspectionRequestUID', 'MapGridUID', 'SourceID', 'CreatedUserUID', 'ModifiedUserUID', 'GPSType', 'GPSSentence', 'SHAPE', 'Comments', 'RevisionComments', 'StatusType', 'AOCType', 'AOCReasonType', 'AOCOther', 'Photo1', 'Photo2', 'Photo3', 'OptionalData1', 'OptionalData2', 'OptionalData3', 'OptionalData4', 'OptionalData5', 'ApprovedByUserUID', 'SubmittedStatusType', 'SubmittedUserUID', 'ResponseStatusType', 'Responsecomments', 'ResponceErrorComments', 'MeterNumber', 'GPSSource', 'GPSTime', 'ChecksumData', 'GPSStatus', 'MasterLeakLogUID', 'ActivityUID'], 'string'],
+            [['ProjectID', 'Revision', 'ActiveFlag', 'ApprovedFlag', 'SubmittedFlag', 'CompletedFlag', 'FixQuality', 'NumberOfSatellites', 'NumberOfGPSAttempts'], 'integer'],
+            [['SrcDTLT', 'SrvDTLT', 'SrvDTLTOffset', 'SrcOpenDTLT', 'SrcClosedDTLT', 'ApprovedDTLT', 'SubmittedDTLT', 'ResponseDTLT', 'CompletedDTLT', 'DateFound'], 'safe'],
+            [['Latitude', 'Longitude', 'HDOP', 'AltitudemetersAboveMeanSeaLevel', 'HeightOfGeoid', 'TimeSecondsSinceLastDGPS', 'Bearing', 'Speed'], 'number']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'AssetAddressAOCID' => 'Asset Address Aocid',
+            'AssetAddressAOCUID' => 'Asset Address Aocuid',
+            'AssetAddressUID' => 'Asset Address Uid',
+            'AssetInspectionUID' => 'Asset Inspection Uid',
+            'InspectionRequestUID' => 'Inspection Request Uid',
+            'MapGridUID' => 'Map Grid Uid',
+            'ProjectID' => 'Project ID',
+            'SourceID' => 'Source ID',
+            'CreatedUserUID' => 'Created User Uid',
+            'ModifiedUserUID' => 'Modified User Uid',
+            'SrcDTLT' => 'Src Dtlt',
+            'SrvDTLT' => 'Srv Dtlt',
+            'SrvDTLTOffset' => 'Srv Dtltoffset',
+            'SrcOpenDTLT' => 'Src Open Dtlt',
+            'SrcClosedDTLT' => 'Src Closed Dtlt',
+            'GPSType' => 'Gpstype',
+            'GPSSentence' => 'Gpssentence',
+            'Latitude' => 'Latitude',
+            'Longitude' => 'Longitude',
+            'SHAPE' => 'Shape',
+            'Comments' => 'Comments',
+            'RevisionComments' => 'Revision Comments',
+            'Revision' => 'Revision',
+            'ActiveFlag' => 'Active Flag',
+            'StatusType' => 'Status Type',
+            'AOCType' => 'Aoctype',
+            'AOCReasonType' => 'Aocreason Type',
+            'AOCOther' => 'Aocother',
+            'Photo1' => 'Photo1',
+            'Photo2' => 'Photo2',
+            'Photo3' => 'Photo3',
+            'OptionalData1' => 'Optional Data1',
+            'OptionalData2' => 'Optional Data2',
+            'OptionalData3' => 'Optional Data3',
+            'OptionalData4' => 'Optional Data4',
+            'OptionalData5' => 'Optional Data5',
+            'ApprovedFlag' => 'Approved Flag',
+            'ApprovedByUserUID' => 'Approved By User Uid',
+            'ApprovedDTLT' => 'Approved Dtlt',
+            'SubmittedFlag' => 'Submitted Flag',
+            'SubmittedStatusType' => 'Submitted Status Type',
+            'SubmittedUserUID' => 'Submitted User Uid',
+            'SubmittedDTLT' => 'Submitted Dtlt',
+            'ResponseStatusType' => 'Response Status Type',
+            'Responsecomments' => 'Responsecomments',
+            'ResponceErrorComments' => 'Responce Error Comments',
+            'ResponseDTLT' => 'Response Dtlt',
+            'CompletedFlag' => 'Completed Flag',
+            'CompletedDTLT' => 'Completed Dtlt',
+            'DateFound' => 'Date Found',
+            'MeterNumber' => 'Meter Number',
+            'GPSSource' => 'Gpssource',
+            'GPSTime' => 'Gpstime',
+            'FixQuality' => 'Fix Quality',
+            'NumberOfSatellites' => 'Number Of Satellites',
+            'HDOP' => 'Hdop',
+            'AltitudemetersAboveMeanSeaLevel' => 'Altitudemeters Above Mean Sea Level',
+            'HeightOfGeoid' => 'Height Of Geoid',
+            'TimeSecondsSinceLastDGPS' => 'Time Seconds Since Last Dgps',
+            'ChecksumData' => 'Checksum Data',
+            'Bearing' => 'Bearing',
+            'Speed' => 'Speed',
+            'GPSStatus' => 'Gpsstatus',
+            'NumberOfGPSAttempts' => 'Number Of Gpsattempts',
+            'MasterLeakLogUID' => 'Master Leak Log Uid',
+            'ActivityUID' => 'Activity Uid',
+        ];
+    }
+}
