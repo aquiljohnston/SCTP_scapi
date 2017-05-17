52173453c0b76f238c187ecc691efec2c2f08551
aquiljohston <aquiljohnston3@gmail.com>
2017-05-17 09:52:37 -0400

    Added order by to aoc division/workcenter dropdown.

--- SCAPI/scapi/modules/v1/modules/pge/controllers/DropdownController.php
+++ SCAPI/scapi/modules/v1/modules/pge/controllers/DropdownController.php
@@ -1464,6 +1464,7 @@ class DropdownController extends Controller
             //todo permission check
 
             $divisionData = WebManagementDropDownAOCDivision::find()
+				->orderBy('Division')
                 ->all();
             $divisionNamePairs = [null => "Select..."];
             $divisionDataSize = count($divisionData);
@@ -1483,6 +1484,7 @@ class DropdownController extends Controller
                 $namePairs[$divisionData[$i]->Division]= $divisionData[$i]->Division;
                 $workCenterData = WebManagementDropDownAOCWorkCenter::find()
                     ->where(['Division'=>$divisionData[$i]->Division])
+					->orderBy('WorkCenter')
                     ->all();
                 for ($j = 0; $j < count($workCenterData); $j++){
                     $workCenterNamePairs[] = $workCenterData[$j]->WorkCenter;
