363b34c29068b735b400184eada5e7dbbe531cb6
Michael Davis <mdavis@southerncrossinc.com>
2017-05-26 15:47:21 -0400

    Added dispatch get surveyors route.

--- SCAPI/scapi/modules/v2/controllers/DispatchController.php
+++ SCAPI/scapi/modules/v2/controllers/DispatchController.php
@@ -10,6 +10,7 @@ use yii\data\Pagination;
 use app\authentication\TokenAuth;
 use app\modules\v2\models\BaseActiveRecord;
 use app\modules\v2\models\AvailableWorkQueue;
+use app\modules\v2\models\SCUser;
 use app\modules\v2\models\AssignedWorkQueue;
 use app\modules\v2\controllers\BaseActiveController;
 use yii\web\ForbiddenHttpException;
@@ -94,14 +95,30 @@ class DispatchController extends Controller
         }
 	}
 
-    public function actionGetSurveyors($workCenter = null, $filter = null, $listPerPage = null, $page = null)
+    public function actionGetSurveyors($filter = null)
     {
-        $users = [
-            [
-                'Name' => 'Patton, Josh',
-                'Division' => 'Las Vegas'
-            ]
-        ];
+        //set db
+		$headers = getallheaders();
+		BaseActiveRecord::setClient($headers['X-Client']);
+			
+		$userQuery = SCUser::find()
+			->select(['UserID', "concat(UserLastName, ', ', UserFirstName) as Name", 'UserName'])
+			->where(['UserActiveFlag' => 1])
+			->andWhere(['<>', 'UserAppRoleType', 'Admin']);
+		
+		if($filter != null)
+		{
+			$userQuery->andFilterWhere([
+			'or',
+			['like', 'UserName', $filter],
+			['like', 'UserFirstName', $filter],
+			['like', 'UserLastName', $filter],
+			]);
+		}
+		
+		$users = $userQuery->asArray()
+			->all();
+		
         $responseArray['users'] = $users;
         //send response
         $response = Yii::$app->response;
@@ -109,4 +126,59 @@ class DispatchController extends Controller
         $response->data = $responseArray;
         return $response;
     }
+	
+	public function actionGetAssigned($filter = null, $listPerPage = 10, $page = 1)
+	{
+		try
+		{
+			//set db
+			$headers = getallheaders();
+			BaseActiveRecord::setClient($headers['X-Client']);
+			
+			$responseArray = [];
+			
+			$assetQuery = AssignedWorkQueue::find()->where(['CompletedFlag' => 0]);
+			
+			// if($filter != null)
+			// {
+				// $assetQuery->andFilterWhere([
+				// 'or',
+				// ['like', 'Division', $filter],
+				// ['like', 'WorkCenter', $filter],
+				// ['like', 'SurveyType', $filter],
+				// ['like', 'FLOC', $filter],
+				// ['like', 'Notification ID', $filter],
+				// ['like', 'ComplianceDueDate', $filter],
+				// ['like', 'SAP Released', $filter],
+				// ['like', 'ComplianceYearMonth', $filter],
+				// ['like', 'PreviousServices', $filter],
+				// ]);
+			// }
+			
+			if($page != null)
+			{
+				//pass query with pagination data to helper method
+				$paginationResponse = BaseActiveController::paginationProcessor($assetQuery, $page, $listPerPage);
+				//use updated query with pagination caluse to get data
+				$assets = $paginationResponse['Query']->orderBy('ComplianceEnd')
+				->all();
+				$responseArray['pages'] = $paginationResponse['pages'];
+				$responseArray['assets'] = $assets;
+			}
+			
+			//create response object
+			$response = Yii::$app->response;
+			$response->format = Response::FORMAT_JSON;
+			$response->data = $responseArray;
+			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+            throw new \yii\web\HttpException(400);
+        }
+	}
 }
