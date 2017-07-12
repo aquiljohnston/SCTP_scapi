98a68cb3c015da1c376775c36867956d9a920dcb
Michael Davis <mdavis@southerncrossinc.com>
2017-07-12 14:49:19 -0400

    Added route to get map assets.

---
+++ SCAPI/scapi/modules/v2/controllers/MapController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\modules\v2\controllers;
+
+use Yii;
+use yii\rest\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\authentication\TokenAuth;
+use app\modules\v2\controllers\BaseActiveController;
+use app\modules\v2\models\BaseActiveRecord;
+use app\modules\v2\models\WebManagementMapView;
+use yii\web\ForbiddenHttpException;
+use yii\web\BadRequestHttpException;
+
+class MapController extends Controller 
+{
+
+	public function behaviors()
+	{
+		$behaviors = parent::behaviors();
+		$behaviors['authenticator'] = 
+		[
+			'class' => TokenAuth::className(),
+		];
+		$behaviors['verbs'] = 
+			[
+                'class' => VerbFilter::className(),
+                'actions' => [
+					'get' => ['get'],
+                ],  
+            ];
+		return $behaviors;	
+	}
+	
+	public function actionGet($mapgrid)
+	{
+		try
+		{
+			//set db
+			$headers = getallheaders();
+			BaseActiveRecord::setClient($headers['X-Client']);
+			
+			//create response format
+			$responseData = [];
+			
+			$assets = WebManagementMapView::find()
+				->where(['MapGrid' => $mapgrid])
+				->orderBy('MapGrid')
+				->all();
+			$responseData = $assets;
+			
+			//send response
+			$response = Yii::$app->response;
+			$response->format = Response::FORMAT_JSON;
+			$response->data = $responseData;
+			return $response;
+		}
+        catch(ForbiddenHttpException $e)
+        {
+            throw new ForbiddenHttpException;
+        }
+        catch(\Exception $e)
+        {
+			BaseActiveController::archiveErrorJson(file_get_contents("php://input"), $e, getallheaders()['X-Client']);
+            throw new \yii\web\HttpException(400);
+        }
+	}
+}
