bee30b8ab5a1300d9580fdd2107ad195a668130b
aquiljohston <aquiljohnston3@gmail.com>
2017-11-22 08:16:06 -0500

    Updated dispatch controller to handle dispatching at the asset level without having to query first by adding section number to the dispatch request. Also updated unassign the accept user id at the asset level to unassign pipeline from a  single user if assigned to two.

--- SCAPI/scapi/modules/v2/controllers/DispatchController.php
+++ SCAPI/scapi/modules/v2/controllers/DispatchController.php
@@ -308,7 +308,7 @@ class DispatchController extends Controller
 						$data['dispatchAsset'][$i]['AssignedUserID'],
 						$createdBy,
 						null,
-						null,
+						$data['dispatchAsset'][$i]['SectionNumber'],
 						$data['dispatchAsset'][$i]['WorkOrderID'],
 						$scheduledDate
 					);
@@ -523,7 +523,9 @@ class DispatchController extends Controller
 				$results = self::processUnassigned(
 					null,
 					null,
-					$data['unassignAsset'][$i]['WorkOrderID']
+					$data['unassignAsset'][$i]['WorkOrderID'],
+					//ternary check if user id is present
+					(array_key_exists('AssignedUserID', $data['unassignAsset'][$i]) ? $data['unassignAsset'][$i]['AssignedUserID'] : null)
 				);
 				$responseData['unassignAsset'][] = $results;
 			}
@@ -551,9 +553,11 @@ class DispatchController extends Controller
 	private static function processDispatch($userID, $createdBy, $mapGrid = null, $section = null, $workOrder = null, $scheduledDate = null)
 	{
 		$results = [];
+		$workOrders = [];
 		//get status code for Assigned work
 		$assignedCode = self::statusCodeLookup('Assigned');
 		$successFlag = 1;
+		$isAsset = 0;
 
 		//pull work orders to update
         if ($scheduledDate == null) {
@@ -564,16 +568,17 @@ class DispatchController extends Controller
                 if ($section != null) {
                     $workOrdersQuery->andWhere(['SectionNumber' => $section]);
                 }
+				 $workOrders = $workOrdersQuery->all();
+				 $workOrdersCount = count($workOrders);
             } else {
-                $workOrdersQuery = AvailableWorkOrder::find()
-                    ->where(['WorkOrderID' => $workOrder]);
+                $isAsset = true;
+				$workOrdersCount = 1;
             }
-            $workOrders = $workOrdersQuery->all();
         } else {
             $workOrders = self::getCgeWorkOrders($mapGrid, $workOrder);
+			$workOrdersCount = count($workOrders);
         }
 		
-		$workOrdersCount = count($workOrders);
 		$db = BaseActiveRecord::getDb();
 		$transaction = $db->beginTransaction();
 		try{
@@ -585,9 +590,16 @@ class DispatchController extends Controller
 				'CreatedDate' => date(BaseActiveController::DATE_FORMAT),
 				'AssignedUserID' => $userID,
 				'WorkQueueStatus' => $assignedCode,
-				'SectionNumber' => $workOrders[$i]->SectionNumber,
-				'WorkOrderID' => $workOrders[$i]->WorkOrderID
 				];
+				
+				//assign workorder/section based on data available
+				if($isAsset){
+					$dataArray['SectionNumber'] = $section;
+					$dataArray['WorkOrderID'] = $workOrder;
+				}else{
+					$dataArray['SectionNumber'] = $workOrders[$i]->SectionNumber;
+					$dataArray['WorkOrderID'] = $workOrders[$i]->WorkOrderID;
+				}
 				
 				if ($scheduledDate != null)
 					$dataArray['ScheduledDispatchDate'] = date(BaseActiveController::DATE_FORMAT,strtotime($scheduledDate));
@@ -610,15 +622,16 @@ class DispatchController extends Controller
 		];
 	}
 	
-	private static function processUnassigned($mapGrid = null, $section = null, $workOrder = null)
+	private static function processUnassigned($mapGrid = null, $section = null, $workOrder = null, $assignedUserID = null)
 	{
 		$successFlag = 0;
 		try{
 			$connection = BaseActiveRecord::getDb();
-			$processJSONCommand = $connection->createCommand("EXECUTE spUnassignWO :MapGrid, :SectionNum , :WorkOrderID");
+			$processJSONCommand = $connection->createCommand("EXECUTE spUnassignWO :MapGrid, :SectionNum , :WorkOrderID, :AssignedUserID");
 			$processJSONCommand->bindParam(':MapGrid', $mapGrid,  \PDO::PARAM_STR);
 			$processJSONCommand->bindParam(':SectionNum', $section,  \PDO::PARAM_INT);
 			$processJSONCommand->bindParam(':WorkOrderID', $workOrder,  \PDO::PARAM_INT);
+			$processJSONCommand->bindParam(':AssignedUserID', $assignedUserID,  \PDO::PARAM_INT);
 			$processJSONCommand->execute();
 			$successFlag = 1;
 		}
@@ -628,6 +641,7 @@ class DispatchController extends Controller
 			'MapGrid' => $mapGrid,
 			'SectionNumber' => $section,
 			'WorkOrderID' => $workOrder,
+			'UserID' => $assignedUserID,
 			'SuccessFlag' => $successFlag
 			]);
 		}
@@ -637,6 +651,7 @@ class DispatchController extends Controller
 			'MapGrid' => $mapGrid,
 			'SectionNumber' => $section,
 			'WorkOrderID' => $workOrder,
+			'UserID' => $assignedUserID,
 			'SuccessFlag' => $successFlag
 		];
 	}
