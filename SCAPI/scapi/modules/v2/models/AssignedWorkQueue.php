427a05b0dcdce910fa8d9633ff6104de033da83f
Essex Igyan <eigyan@southerncrossinc.com>
2017-12-06 16:09:02 -0500

    Project - 521 - API - Update Get Work Queue Route

--- SCAPI/scapi/modules/v2/models/AssignedWorkQueue.php
+++ SCAPI/scapi/modules/v2/models/AssignedWorkQueue.php
@@ -122,6 +122,10 @@ class AssignedWorkQueue extends \app\modules\v2\models\BaseActiveRecord
             'AssignedByID' => 'Assigned By ID',
             'AssignedToID' => 'Assigned To ID',
             'Address' => 'Address',
+            'BillingCode' => 'Billing Code',
+            'MeterLocation' => 'Meter Locaation',
+            'PipelineFootage' => 'Pipeline Footage',
+            'SpecialInstructions' => 'Special Instructions',
         ];
     }
 }
