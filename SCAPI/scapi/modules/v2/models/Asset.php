e73633410bc4e65321ec0b49cefffde489a639d5
tzhang <tzhang@8l9z362.speardom.com>
2017-12-11 14:40:08 -0500

    updated Asset model

--- SCAPI/scapi/modules/v2/models/Asset.php
+++ SCAPI/scapi/modules/v2/models/Asset.php
@@ -45,6 +45,7 @@ use Yii;
  * @property double $Speed
  * @property integer $NumberOfGPSAttempts
  * @property integer $Zip
+ * @property string $MeterLocation
  *
  * @property UserTb $createdUser
  */
@@ -64,7 +65,7 @@ class Asset extends \app\modules\v2\models\BaseActiveRecord
     public function rules()
     {
         return [
-            [['AssetTabletID', 'MapGrid', 'HouseNo', 'Street', 'Apt', 'City', 'State', 'ReverseGeoLocationString', 'MeterID', 'PipelineType', 'Grade1ReleaseReasonType', 'Comments', 'Photo1Path', 'Inspection', 'GPSType', 'GPSSentence', 'GPSTime', 'CheckSumData', 'Zip'], 'string'],
+            [['AssetTabletID', 'MapGrid', 'HouseNo', 'Street', 'Apt', 'City', 'State', 'ReverseGeoLocationString', 'MeterID', 'PipelineType', 'Grade1ReleaseReasonType', 'Comments', 'Photo1Path', 'Inspection', 'GPSType', 'GPSSentence', 'GPSTime', 'CheckSumData', 'Zip', 'MeterLocation'], 'string'],
             [['InspectionID', 'CreatedUserID', 'AOCs', 'NumberOfSatellites', 'NumberOfGPSAttempts'], 'integer'],
             [['Grade1ReleaseDateTime', 'SrcDTLT', 'SrvDTLT', 'SrvDTLTOffset'], 'safe'],
             [['Latitude', 'Longitude', 'HDOP', 'AltitudeMetersAboveMeanSeaLevel', 'HeightOfGeoid', 'TimeSecondsSinceLastDGPS', 'Bearing', 'Speed', 'FixQuality'], 'number'],
@@ -117,6 +118,7 @@ class Asset extends \app\modules\v2\models\BaseActiveRecord
             'NumberOfGPSAttempts' => 'Number Of Gpsattempts',
             'Zip' => 'Zip',
             'MeterNumber' => 'Meter Number',
+            'MeterLocation' => 'Meter Location',
         ];
     }
 
