d49ad4c0a797fd88e0231796cdb70efcec2074f2
Michael Davis <mdavis@southerncrossinc.com>
2018-01-19 09:47:43 -0500

    Created new rbac DbManager in v2 module to prevent version conflicts.

---
+++ SCAPI/scapi/modules/v2/rbac/PgeDbManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\modules\v2\rbac;
+
+use Yii;
+use yii\rbac\DbManager;
+use app\modules\v2\models\BaseActiveRecord;
+use app\modules\v2\controllers\BaseActiveController;
+
+class PgeDbManager extends DbManager
+{
+	public $db = '';
+	/**
+     * @var string the name of the table storing authorization items. Defaults to "auth_item".
+     */
+	public $itemTable = '{{auth_item}}';
+	/**
+     * @var string the name of the table storing authorization item hierarchy. Defaults to "auth_item_child".
+     */
+	public $itemChildTable = '{{auth_item_child}}';
+	/**
+     * @var string the name of the table storing authorization item assignments. Defaults to "auth_assignment".
+     */
+    public $assignmentTable = '{{auth_assignment}}';
+    /**
+     * @var string the name of the table storing rules. Defaults to "auth_rule".
+     */
+    public $ruleTable = '{{auth_rule}}';
+	
+	public function __construct($currentDb)
+	{
+		$this->db = $currentDb;
+	}
+}
