098f63adfe6251dbade4f6e3e4be1d399f2b7d99
tzhang <tzhang@8l9z362.speardom.com>
2018-01-26 11:22:46 -0500

    added route to get ChartOfAccountType from CT DB

---
+++ SCAPI/scapi/modules/v2/models/ChartOfAccountType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tzhang
+ * Date: 2018/1/26
+ * Time: 11:12
+ */
+
+namespace app\modules\v2\models;
+
+/**
+ * This is the model class for table "ChartOfAccountTb".
+ *
+ * @property integer $ChartOfAccountID
+ * @property string $ChartOfAccountDescription
+ * @property string $ChartOfAccountUnits
+ * @property string $ChartOfAccountUsage
+ */
+
+class ChartOfAccountType extends BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ChartOfAccountTb';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['ChartOfAccountDescription', 'ChartOfAccountUnits', 'ChartOfAccountUsage'], 'string'],
+            [['ChartOfAccountID'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ChartOfAccountID' => 'Chart Of Account ID',
+            'ChartOfAccountDescription' => 'Chart Of Account Description',
+            'ChartOfAccountUnits' => 'Chart Of Account Units',
+            'ChartOfAccountUsage' => 'Chart Of Account Usage'
+        ];
+    }
+}
