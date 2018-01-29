f9b1bdcb3de70cd51545913df39cd6a09fb667fc
aquiljohston <aquiljohnston3@gmail.com>
2018-01-29 10:38:10 -0500

    Implemented base v3 config SCC-384. TODO merge web/tablet dropdown routes.

---
+++ SCAPI/scapi/modules/v3/rbac/ClientDbManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\modules\v3\rbac;
+
+use Yii;
+use yii\rbac\DbManager;
+
+class ClientDbManager extends DbManager
+{
+	public $db = '';
+	/**
+     * @var string the name of the table storing authorization items. Defaults to "auth_item".
+     */
+	public $itemTable = '{{auth_item}}';
+	/**
+     * @var string the name of the table storing authorization item hierarchy. Defaults to "auth_item_child".
+     */
+	public $itemChildTable = '{{auth_item_child}}';
+	/**
+     * @var string the name of the table storing authorization item assignments. Defaults to "auth_assignment".
+     */
+    public $assignmentTable = '{{auth_assignment}}';
+    /**
+     * @var string the name of the table storing rules. Defaults to "auth_rule".
+     */
+    public $ruleTable = '{{auth_rule}}';
+	
+	public function __construct($currentDb)
+	{
+		$this->db = $currentDb;
+	}
+}
