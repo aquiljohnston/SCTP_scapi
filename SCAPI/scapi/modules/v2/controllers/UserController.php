f0f8249ac580ac31a5776f0fbfa008cf3b5bafbe
Michael Davis <mdavis@southerncrossinc.com>
2018-02-15 10:31:17 -0500

    Removed call to stub route that was removed previously. SCC-671

--- SCAPI/scapi/modules/v2/controllers/UserController.php
+++ SCAPI/scapi/modules/v2/controllers/UserController.php
@@ -559,8 +559,7 @@ class UserController extends BaseActiveController
 				$projectUserID = BaseActiveController::getClientUser($projectModel->ProjectUrlPrefix)->UserID;
 				$projectUserName = BaseActiveController::getClientUser($projectModel->ProjectUrlPrefix)->UserName;
 				
-				$projectTask = Yii::$app->runAction('v2/task/get-project-task');
-				$projectUserTask = Yii::$app->runAction('v2/task/get-project-user-task');
+				$projectTask = TaskController::GetProjectTask($projectID);//Yii::$app->runAction('v2/task/get-project-task', ['projectID'=>$projectID]);
 				
 				//set client back to ct after external call
 				BaseActiveRecord::setClient(BaseActiveController::urlPrefix());
