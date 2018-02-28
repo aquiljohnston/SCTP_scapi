2a9ce1290cbe31393b18d7c5fce4f1556c5851b4
aquiljohston <aquiljohnston3@gmail.com>
2018-02-28 13:06:48 -0500

    Moved Task Routes from time card controller into task controller. CT-659

--- SCAPI/scapi/modules/v2/controllers/TaskController.php
+++ SCAPI/scapi/modules/v2/controllers/TaskController.php
@@ -8,6 +8,7 @@ use Yii;
 use yii\rest\Controller;
 use app\modules\v2\models\Task;
 use app\modules\v2\models\TaskAndProject;
+use app\modules\v2\models\ChartOfAccountType;
 use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\modules\v2\authentication\TokenAuth;
@@ -31,11 +32,14 @@ class TaskController extends Controller
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'get-all-task' => ['get'],
+					'create-task-entry' => ['post'],
+					'get-charge-of-account-type' => ['get'],
                 ],  
             ];
 		return $behaviors;	
 	}
 	
+	//does this need to be public?
 	public static function GetProjectTask($projectID)
 	{
         //set db target
@@ -90,5 +94,85 @@ class TaskController extends Controller
             BaseActiveController::archiveWebErrorJson('actionGetTasks', $e, getallheaders()['X-Client']);
             throw new \yii\web\HttpException(400);
         }
+    }
+	
+	/**
+     * Get ChargeOfAccountType From CT DB
+     * @return mixed
+     */
+    public function actionGetChargeOfAccountType(){
+        //set db target
+        ChartOfAccountType::setClient(BaseActiveController::urlPrefix());
+
+        $chartOfAccountType = ChartOfAccountType::find()
+            ->all();
+
+        $namePairs = [];
+        $codesSize = count($chartOfAccountType);
+
+        for($i=0; $i < $codesSize; $i++)
+        {
+            $namePairs[$chartOfAccountType[$i]->ChartOfAccountID]= $chartOfAccountType[$i]->ChartOfAccountDescription;
+        }
+
+
+        $response = Yii::$app ->response;
+        $response -> format = Response::FORMAT_JSON;
+        $response -> data = $namePairs;
+
+        return $response;
+    }
+	
+	  /**
+     * Create New Task Entry in CT DB
+     * @return mixed
+     * @throws \yii\web\HttpException
+     */
+	 //this should be in the task controller...
+    public function actionCreateTaskEntry()
+    {
+        $successFlag = 0;
+        try {
+            //set db target
+            BaseActiveRecord::setClient(BaseActiveController::urlPrefix());
+
+            //get body data
+            $body = file_get_contents("php://input");
+            $data = json_decode($body, true);
+
+            // set up db connection
+            $connection = BaseActiveRecord::getDb();
+            $processJSONCommand = $connection->createCommand("EXECUTE spAddActivityAndTime :TimeCardID, :TaskName , :Date, :StartTime, :EndTime, :CreatedByUserName, :ChargeOfAccountType");
+            $processJSONCommand->bindParam(':TimeCardID', $data['TimeCardID'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':TaskName', $data['TaskName'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':Date', $data['Date'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':StartTime', $data['StartTime'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':EndTime', $data['EndTime'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':CreatedByUserName', $data['CreatedByUserName'], \PDO::PARAM_STR);
+            $processJSONCommand->bindParam(':ChargeOfAccountType', $data['ChargeOfAccountType'], \PDO::PARAM_STR);
+            $processJSONCommand->execute();
+            $successFlag = 1;
+
+        } catch (\Exception $e) {
+            BaseActiveController::archiveWebErrorJson(file_get_contents("php://input"), $e, getallheaders()['X-Client'], [
+                'TimeCardID' => $data['TimeCardID'],
+                'TaskName' => $data['TaskName'],
+                'Date' => $data['Date'],
+                'StartTime' => $data['StartTime'],
+                'EndTime' => $data['EndTime'],
+                'CreatedByUserName' => $data['CreatedByUserName'],
+                'ChargeOfAccountType' => $data['ChargeOfAccountType'],
+                'SuccessFlag' => $successFlag
+            ]);
+        }
+
+        //build response format
+        $dataArray =  [
+            'TimeCardID' => $data['TimeCardID'],
+            'SuccessFlag' => $successFlag
+        ];
+        $response = Yii::$app->response;
+        $response->format = Response::FORMAT_JSON;
+        $response->data = $dataArray;
     }
 }
