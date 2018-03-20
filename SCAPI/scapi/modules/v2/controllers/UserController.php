e4553e3c49e1f8906ce4507baf36807dc94505c4
Michael Davis <mdavis@southerncrossinc.com>
2018-03-20 11:46:47 -0400

    Removed call to function in task controller that is not part of this release. Will be added back in with ct2. SCC-671

--- SCAPI/scapi/modules/v2/controllers/UserController.php
+++ SCAPI/scapi/modules/v2/controllers/UserController.php
@@ -560,7 +560,6 @@ class UserController extends BaseActiveController
 				$projectUserID = BaseActiveController::getClientUser($projectModel->ProjectUrlPrefix)->UserID;
 				$projectUserName = BaseActiveController::getClientUser($projectModel->ProjectUrlPrefix)->UserName;
 				
-				$projectTask = TaskController::GetProjectTask($projectID);//Yii::$app->runAction('v2/task/get-project-task', ['projectID'=>$projectID]);
 				
 				//set client back to ct after external call
 				BaseActiveRecord::setClient(BaseActiveController::urlPrefix());
