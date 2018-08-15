c7f601ddf82b572361bf4cf543e25e9882a2ed2c
Michael Davis <mdavis@southerncrossinc.com>
2018-08-15 13:01:01 -0400

    Applied auth header checks to v3 as well. SCC-965

--- SCAPI/scapi/modules/v3/authentication/TokenAuth.php
+++ SCAPI/scapi/modules/v3/authentication/TokenAuth.php
@@ -20,35 +20,28 @@ class TokenAuth extends AuthMethod
 		
         $token = $request->getAuthUser();
 		
-		//check for client header
-		$headers = getAllHeaders();
-		try
-		{
-			$headers['X-Client'];
-		}
-		catch(ErrorException $e)
-		{	
-			throw new \yii\web\HttpException(400, 'Client Header Not Found.');
-		}
-		
-		if ($token !== null) {
+		if ($token !== null && $token !== '') {
 			Yii::$app->user->checkTimeout($token);
-			try 
-			{
+			try {
 				$identity = SCUser::findIdentityByAccessToken($token);
-				if ($identity === null) 
-				{
+				if ($identity !== null) {
+					//check for client header
+					try {
+						getAllHeaders()['X-Client'];
+					} catch(ErrorException $e) {	
+						BaseActiveController::archiveErrorJson(file_get_contents("php://input"), $e, BaseActiveController::urlPrefix());
+						throw new \yii\web\HttpException(400, 'Client Header Not Found.');
+					}
+					return $identity;
+				} else { 
 					//TODO move string to constants when version is created
 					throw new \yii\web\HttpException(401, 'You are requesting with invalid credentials.');
 				}
-				return $identity;
-			}
-			catch (\Exception $e) 
-			{
+				
+			} catch (\Exception $e) {
 				Yii::warning("Valid token not found.");
 			}
 		}
-
         return null;
     }
 
