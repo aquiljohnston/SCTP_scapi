4f3989ea4ff8db28d07efa7d930a2ae3a81db922
Michael Davis <mdavis@southerncrossinc.com>
2018-10-23 16:24:59 -0400

    API refactoring to improve get assigned performance. SCC-1051

--- SCAPI/scapi/modules/v2/models/AssignedWorkQueueByMapGrid.php
+++ SCAPI/scapi/modules/v2/models/AssignedWorkQueueByMapGrid.php
@@ -5,66 +5,79 @@ namespace app\modules\v2\models;
 use Yii;
 
 /**
- * This is the model class for table "vAssignedWorkQueueByMapGrid".
+ * This is the model class for table "vAssignedWorkQueueByMapGrid2".
  *
  * @property string $MapGrid
+ * @property string $AssignedUser
  * @property string $ComplianceStart
  * @property string $ComplianceEnd
- * @property integer $InspectionAttemptcounter
- * @property integer $SectionFlag
- * @property integer $AssignedWorkOrderCount
+ * @property int $InspectionAttemptcounter
+ * @property int $SectionFlag
+ * @property int $AssignedWorkOrderCount
+ * @property int $InProgressFlag
  * @property string $AssignedCount
- * @property string $UIDList
- * @property string $SearchString
  * @property string $Percent Completed
- * @property integer $Total
- * @property integer $Remaining
+ * @property int $Total
+ * @property int $Remaining
+ * @property string $BillingCode
+ * @property string $InspectionType
+ * @property string $OfficeName
+ * @property string $UserFirstName
+ * @property string $UserLastName
+ * @property string $UserName
+ * @property string $ComplianceStartDate
+ * @property string $CompliancenEndDate
  */
 class AssignedWorkQueueByMapGrid extends \app\modules\v2\models\BaseActiveRecord
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public static function tableName()
     {
-        return 'vAssignedWorkQueueByMapGrid';
+        return 'vAssignedWorkQueueByMapGrid2';
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['MapGrid', 'AssignedCount', 'UIDList', 'SearchString'], 'string'],
-            [['ComplianceStart', 'ComplianceEnd'], 'safe'],
-            [['InspectionAttemptcounter', 'SectionFlag', 'AssignedWorkOrderCount', 'Total', 'Remaining'], 'integer'],
-            [['SectionFlag', 'AssignedCount'], 'required'],
+            [['MapGrid', 'AssignedUser', 'AssignedCount', 'BillingCode', 'InspectionType', 'OfficeName', 'UserFirstName', 'UserLastName', 'UserName'], 'string'],
+            [['ComplianceStart', 'ComplianceEnd', 'ComplianceStartDate', 'CompliancenEndDate'], 'safe'],
+            [['InspectionAttemptcounter', 'SectionFlag', 'AssignedWorkOrderCount', 'InProgressFlag', 'Total', 'Remaining'], 'integer'],
+            [['SectionFlag', 'InProgressFlag', 'AssignedCount'], 'required'],
             [['Percent Completed'], 'number'],
         ];
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function attributeLabels()
     {
         return [
             'MapGrid' => 'Map Grid',
+            'AssignedUser' => 'Assigned User',
             'ComplianceStart' => 'Compliance Start',
             'ComplianceEnd' => 'Compliance End',
             'InspectionAttemptcounter' => 'Inspection Attemptcounter',
             'SectionFlag' => 'Section Flag',
             'AssignedWorkOrderCount' => 'Assigned Work Order Count',
+            'InProgressFlag' => 'In Progress Flag',
             'AssignedCount' => 'Assigned Count',
-            'UIDList' => 'Uidlist',
-            'SearchString' => 'Search String',
             'Percent Completed' => 'Percent  Completed',
             'Total' => 'Total',
             'Remaining' => 'Remaining',
-            'InspectionType' => 'Inspection Type',
             'BillingCode' => 'Billing Code',
+            'InspectionType' => 'Inspection Type',
             'OfficeName' => 'Office Name',
+            'UserFirstName' => 'User First Name',
+            'UserLastName' => 'User Last Name',
+            'UserName' => 'User Name',
+            'ComplianceStartDate' => 'Compliance Start Date',
+            'CompliancenEndDate' => 'Compliancen End Date',
         ];
     }
 }
