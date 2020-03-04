3eefd54783c46ca9a3d10a3f55a40577a5507277
Michael Davis <mdavis@speardom.com>
2020-03-04 08:57:04 -0500

    Added new v3 get me route with ABC codes. SCC-1940

---
+++ SCAPI/scapi/modules/v3/models/ABCCodes.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\modules\v3\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "refABCCodes".
+ *
+ * @property int $ABCCodeID
+ * @property int $ProjectID
+ * @property string $TaskID
+ * @property int $IsActive
+ * @property int $IsSource
+ * @property string $ABCCodeLabel
+ * @property string $ABCCodeDescription
+ * @property int $ResultTypeID
+ * @property string $ReportingTaskID
+ * @property string $CreatedDate
+ * @property string $LastRefreshDate
+ *
+ * @property TTaskOut[] $tTaskOuts
+ */
+class ABCCodes extends \app\modules\v3\models\BaseActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'refABCCodes';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['ProjectID', 'IsActive', 'IsSource', 'ResultTypeID'], 'integer'],
+            [['TaskID', 'ABCCodeLabel', 'ABCCodeDescription', 'ReportingTaskID'], 'string'],
+            [['CreatedDate', 'LastRefreshDate'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ABCCodeID' => 'Abccode ID',
+            'ProjectID' => 'Project ID',
+            'TaskID' => 'Task ID',
+            'IsActive' => 'Is Active',
+            'IsSource' => 'Is Source',
+            'ABCCodeLabel' => 'Abccode Label',
+            'ABCCodeDescription' => 'Abccode Description',
+            'ResultTypeID' => 'Result Type ID',
+            'ReportingTaskID' => 'Reporting Task ID',
+            'CreatedDate' => 'Created Date',
+            'LastRefreshDate' => 'Last Refresh Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTTaskOuts()
+    {
+        return $this->hasMany(TaskOut::className(), ['ABCCodeID' => 'ABCCodeID']);
+    }
+}
