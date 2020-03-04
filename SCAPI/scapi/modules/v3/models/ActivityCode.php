495c6803483de0e98348c980d9c90ebdc8c9ce38
aquiljohston <aquiljohnston3@gmail.com>
2020-03-04 08:57:04 -0500

    Added new v3 get me route with ABC codes. SCC-1940

---
+++ SCAPI/scapi/modules/v3/models/ActivityCode.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\modules\v3\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ActivityCodeTb".
+ *
+ * @property integer $ActivityCodeID
+ * @property string $ActivityCodeType
+ * @property string $ActivityCodeDescription
+ * @property string $ActivityArchiveFlag
+ * @property string $ActivityCodeCreateDate
+ * @property string $ActivityCodeCreatedBy
+ * @property string $ActivityModifiedDate
+ * @property string $ActivityModifiedBy
+ */
+class ActivityCode extends \app\modules\v3\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'ActivityCodeTb';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['ActivityCodeType', 'ActivityCodeDescription', 'ActivityCodeCreatedBy', 'ActivityModifiedBy', 'ActivityArchiveFlag'], 'string'],
+            [['ActivityCodeCreateDate', 'ActivityModifiedDate'], 'safe']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ActivityCodeID' => 'Activity Code ID',
+            'ActivityCodeType' => 'Activity Code Type',
+            'ActivityCodeDescription' => 'Activity Code Description',
+			'ActivityArchiveFlag' => 'Activity Archive Flag',
+            'ActivityCodeCreateDate' => 'Activity Code Create Date',
+            'ActivityCodeCreatedBy' => 'Activity Code Created By',
+            'ActivityModifiedDate' => 'Activity Modified Date',
+            'ActivityModifiedBy' => 'Activity Modified By',
+        ];
+    }
+}
