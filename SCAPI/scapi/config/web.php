4faf274a22dbfd340537b1d1719fd5fe600617b5
Michael Davis <mdavis@speardom.com>
2020-04-10 15:54:43 -0400

    Created yii command implementation of process ftp script to leverage swift mailer for email notifications. SCC-1999

--- SCAPI/scapi/config/web.php
+++ SCAPI/scapi/config/web.php
@@ -51,7 +51,15 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+			'useFileTransport'=>false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.office365.com',
+                'username' => 'SC_Automated_Reporting@southerncrossinc.com',
+                'password' => '~!3175Scc30071',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
