fa0c1e3ca9fd75ad24e44f49b3b9b12d8f65c605
Michael Davis <mdavis@speardom.com>
2020-06-12 08:06:05 -0400

    Updated per diem script to handle date ranges properly and prevent failure on db constraint. SCC-2085

---
+++ SCAPI/scapi/modules/v3/models/Users.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\modules\v3\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "vUsers".
+ *
+ * @property integer $UserID
+ * @property string $UserName
+ * @property string $UserFirstName
+ * @property string $UserLastName
+ * @property string $UserEmployeeType
+ * @property string $UserAppRoleType
+ */
+class Users extends \app\modules\v3\models\BaseActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vUsers';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['UserName', 'UserFirstName', 'UserLastName', 'UserEmployeeType', 'UserAppRoleType'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'UserID' => 'User ID',
+            'UserName' => 'User Name',
+            'UserFirstName' => 'User First Name',
+            'UserLastName' => 'User Last Name',
+            'UserEmployeeType' => 'User Employee Type',
+            'UserAppRoleType' => 'User App Role Type',
+        ];
+    }
+}
