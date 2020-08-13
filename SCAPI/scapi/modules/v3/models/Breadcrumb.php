773f310eb586f0417131095264c20eceae3ccee5
Michael Davis <mdavis@speardom.com>
2020-08-13 08:55:14 -0400

    Updated breadcrumb model to add task and project id fields. SCC-2215

--- SCAPI/scapi/modules/v3/models/Breadcrumb.php
+++ SCAPI/scapi/modules/v3/models/Breadcrumb.php
@@ -8,40 +8,45 @@ use Yii;
  * This is the model class for table "BreadcrumbTb".
  *
  * @property int $BreadcrumbID
- * @property string $BreadcrumbUID
- * @property string $BreadcrumbActivityUID
- * @property string $BreadcrumbSourceID
- * @property double $BreadcrumbLatitude
- * @property double $BreadcrumbLongitude
- * @property double $BreadcrumbSpeed
- * @property string $BreadcrumbHeading
- * @property string $BreadcrumbDeviceID
- * @property string $BreadcrumbGPSAccuracy
- * @property string $BreadcrumbGPSType
- * @property string $BreadcrumbGPSSentence
- * @property string $BreadcrumbShape
- * @property string $BreadcrumbWorkQueueFilter
- * @property string $BreadcrumbActivityType
- * @property int $BreadcrumbTrackingGroupID
- * @property int $BreadcrumbSatellites
- * @property double $BreadcrumbAltitude
- * @property string $BreadcrumbMapPlat
- * @property int $BreadcrumbArchiveFlag
- * @property string $BreadcrumbComments
- * @property string $BreadcrumbCreatedUserUID
- * @property string $BreadcrumbSrcDTLT
- * @property string $BreadcrumbSrvDTLT
- * @property string $BreadcrumbSrvDTLTOffset
- * @property string $BreadcrumbCreatedDate
- * @property double $BreadcrumbBatteryLevel
- * @property string $BreadcrumbGPSTime
- * @property int $IsStationary
- * @property string $PaceOfTravel
- * @property int $IsDistanceBased
- * @property string $DistanceTraveled
- * @property double $OriginBreadcrumbLatitude
- * @property double $OriginBreadcrumbLongitude
- * @property string $SpeedAttribute
+ * @property string|null $BreadcrumbUID
+ * @property string|null $BreadcrumbActivityUID
+ * @property string|null $BreadcrumbSourceID
+ * @property float $BreadcrumbLatitude
+ * @property float $BreadcrumbLongitude
+ * @property float|null $BreadcrumbSpeed
+ * @property string|null $BreadcrumbHeading
+ * @property string|null $BreadcrumbDeviceID
+ * @property string|null $BreadcrumbGPSAccuracy
+ * @property string|null $BreadcrumbGPSType
+ * @property string|null $BreadcrumbGPSSentence
+ * @property string|null $BreadcrumbShape
+ * @property string|null $BreadcrumbWorkQueueFilter
+ * @property string|null $BreadcrumbActivityType
+ * @property int|null $BreadcrumbTrackingGroupID
+ * @property float|null $BreadcrumbSatellites
+ * @property float|null $BreadcrumbAltitude
+ * @property string|null $BreadcrumbMapPlat
+ * @property int|null $BreadcrumbArchiveFlag
+ * @property string|null $BreadcrumbComments
+ * @property string|null $BreadcrumbCreatedUserUID
+ * @property string|null $BreadcrumbSrcDTLT
+ * @property string|null $BreadcrumbSrvDTLT
+ * @property string|null $BreadcrumbSrvDTLTOffset
+ * @property string|null $BreadcrumbCreatedDate
+ * @property float|null $BreadcrumbBatteryLevel
+ * @property string|null $BreadcrumbGPSTime
+ * @property int|null $IsStationary
+ * @property float|null $PaceOfTravel
+ * @property int|null $IsDistanceBased
+ * @property float|null $DistanceTraveled
+ * @property float|null $OriginBreadcrumbLatitude
+ * @property float|null $OriginBreadcrumbLongitude
+ * @property string|null $SpeedAttribute
+ * @property int|null $ProjectID
+ * @property int|null $TaskID
+ *
+ * @property ProjectTb $project
+ * @property RefTask $task
  */
 class Breadcrumb extends \app\modules\v3\models\BaseActiveRecord
 {
@@ -61,8 +66,10 @@ class Breadcrumb extends \app\modules\v3\models\BaseActiveRecord
         return [
             [['BreadcrumbUID', 'BreadcrumbActivityUID', 'BreadcrumbSourceID', 'BreadcrumbHeading', 'BreadcrumbDeviceID', 'BreadcrumbGPSAccuracy', 'BreadcrumbGPSType', 'BreadcrumbGPSSentence', 'BreadcrumbShape', 'BreadcrumbWorkQueueFilter', 'BreadcrumbActivityType', 'BreadcrumbMapPlat', 'BreadcrumbComments', 'BreadcrumbCreatedUserUID', 'SpeedAttribute'], 'string'],
             [['BreadcrumbLatitude', 'BreadcrumbLongitude', 'BreadcrumbSpeed', 'BreadcrumbSatellites', 'BreadcrumbAltitude', 'BreadcrumbBatteryLevel', 'PaceOfTravel', 'DistanceTraveled', 'OriginBreadcrumbLatitude', 'OriginBreadcrumbLongitude'], 'number'],
-            [['BreadcrumbTrackingGroupID', 'BreadcrumbArchiveFlag', 'IsStationary', 'IsDistanceBased'], 'integer'],
+            [['BreadcrumbTrackingGroupID', 'BreadcrumbArchiveFlag', 'IsStationary', 'IsDistanceBased', 'ProjectID', 'TaskID'], 'integer'],
             [['BreadcrumbSrcDTLT', 'BreadcrumbSrvDTLT', 'BreadcrumbSrvDTLTOffset', 'BreadcrumbCreatedDate', 'BreadcrumbGPSTime'], 'safe'],
+            [['ProjectID'], 'exist', 'skipOnError' => true, 'targetClass' => Project::className(), 'targetAttribute' => ['ProjectID' => 'ProjectID']],
+            [['TaskID'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['TaskID' => 'TaskID']],
         ];
     }
 
@@ -107,6 +114,28 @@ class Breadcrumb extends \app\modules\v3\models\BaseActiveRecord
             'OriginBreadcrumbLatitude' => 'Origin Breadcrumb Latitude',
             'OriginBreadcrumbLongitude' => 'Origin Breadcrumb Longitude',
             'SpeedAttribute' => 'Speed Attribute',
+            'ProjectID' => 'Project ID',
+            'TaskID' => 'Task ID',
         ];
+    }
+
+    /**
+     * Gets query for [[Project]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProject()
+    {
+        return $this->hasOne(Project::className(), ['ProjectID' => 'ProjectID']);
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['TaskID' => 'TaskID']);
     }
 }
